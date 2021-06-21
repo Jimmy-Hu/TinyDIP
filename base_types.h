a137e8daf011efbf6ae8ec008989ccf4c1f5e55e
jimmyhu <60071jimmy@gmail.com>
2021-06-21 23:50:47 +0800

    Add base_types.h
    Add base_types.h

---
+++ base_types.h
@@ -0,0 +1,32 @@
+/* Develop by Jimmy Hu */
+
+#ifndef BASE_H
+#define BASE_H
+
+#include <cmath>
+#include <cstdbool>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#define MAX_PATH 256
+#define FILE_ROOT_PATH "./"
+
+#define True true
+#define False false
+
+typedef unsigned char BYTE;
+
+typedef struct RGB
+{
+    unsigned char channels[3];
+} RGB;
+
+typedef BYTE GrayScale;
+
+typedef struct HSV
+{
+    long double channels[3];    //  Range: 0 <= H < 360, 0 <= S <= 1, 0 <= V <= 255
+}HSV;
+
+#endif
