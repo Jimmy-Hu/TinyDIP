ac0cb361b94bd6261e9989b5ff53dbdddbcdaa4e
jimmyhu <60071jimmy@gmail.com>
2021-06-23 13:24:51 +0800

    Add image_operations.h
    Add image_operations.h

---
+++ image_operations.h
@@ -0,0 +1,11 @@
+#include "base_types.h"
+
+
+namespace TinyDIP
+{
+    template<ElementT>
+    Image<ElementT> copyResizeBicubic(Image<ElementT> const& image, int width, int height)
+    {
+        
+    }
+}
