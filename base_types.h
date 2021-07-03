e0e4f0e6927120ce585288d5878b4098e221c894
jimmyhu <60071jimmy@gmail.com>
2021-07-03 23:58:28 +0800

    Update base_types.h
    Update base_types.h

--- base_types.h
+++ base_types.h
@@ -12,16 +12,14 @@
 #define MAX_PATH 256
 #define FILE_ROOT_PATH "./"
 
-typedef unsigned char BYTE;
-
-typedef struct RGB
+struct RGB
 {
     unsigned char channels[3];
 };
 
-typedef BYTE GrayScale;
+using GrayScale = unsigned char;
 
-typedef struct HSV
+struct HSV
 {
     long double channels[3];    //  Range: 0 <= H < 360, 0 <= S <= 1, 0 <= V <= 255
 };
