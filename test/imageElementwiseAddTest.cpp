f0062ed0553d2217d8f762bc92d514d2f75d6835
jimmyhu <60071jimmy@gmail.com>
2021-07-04 00:30:10 +0800

    Add imageElementwiseAddTest.cpp
    Add imageElementwiseAddTest.cpp

---
+++ test/imageElementwiseAddTest.cpp
@@ -0,0 +1,31 @@
+/* Developed by Jimmy Hu */
+
+//  compile command:
+//  clang++ -std=c++20 -Xpreprocessor -fopenmp -I/usr/local/include -L/usr/local/lib -lomp  imageElementwiseAddTest.cpp -L /usr/local/Cellar/llvm/10.0.0_3/lib/ -lm -O3 -o imageElementwiseAddTest -v
+//  https://stackoverflow.com/a/61821729/6667035
+//  clear && rm -rf ./imageElementwiseAddTest && g++-11 -std=c++20 -O4 -ffast-math -funsafe-math-optimizations -std=c++20 -fpermissive -H --verbose -Wall imageElementwiseAddTest.cpp -o imageElementwiseAddTest 
+
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void imageElementwiseAddTest();
+
+int main()
+{
+    imageElementwiseAddTest();
+    return 0;
+}
+
+void imageElementwiseAddTest()
+{
+    auto test = TinyDIP::Image<int>(10, 10, 1);
+    test+=test;
+    test.print();
+
+    auto test2 = TinyDIP::Image<int>(11, 11, 1);
+    test+=test2;
+    test.print();
+}
+
