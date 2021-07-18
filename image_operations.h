a6c1b9cf8bdbb8af41b36f7c7e2335959465b99a
jimmyhu <60071jimmy@gmail.com>
2021-07-18 15:52:52 +0800

    Move bicubicPolate function definition
    Move bicubicPolate function definition

--- image_operations.h
+++ image_operations.h
@@ -34,7 +34,19 @@ namespace TinyDIP
         return D + frac * (C + frac * (B + frac * A));
     }
 
+    template<class InputT = float, class ElementT>
+    constexpr static auto bicubicPolate(const ElementT* const ndata, const InputT fracx, const InputT fracy)
+    {
+        auto x1 = cubicPolate( ndata[0], ndata[1], ndata[2], ndata[3], fracx );
+        auto x2 = cubicPolate( ndata[4], ndata[5], ndata[6], ndata[7], fracx );
+        auto x3 = cubicPolate( ndata[8], ndata[9], ndata[10], ndata[11], fracx );
+        auto x4 = cubicPolate( ndata[12], ndata[13], ndata[14], ndata[15], fracx );
 
+        return std::clamp(
+                    cubicPolate( x1, x2, x3, x4, fracy ), 
+                    static_cast<InputT>(std::numeric_limits<ElementT>::min()),
+                    static_cast<InputT>(std::numeric_limits<ElementT>::max()));
+    }
 
     template<class FloatingType = float, class ElementT>
     Image<ElementT> copyResizeBicubic(Image<ElementT>& image, size_t width, size_t height)
@@ -70,20 +82,6 @@ namespace TinyDIP
             }
         }
         return output;
-    }
-
-    template<class InputT = float, class ElementT>
-    constexpr static auto bicubicPolate(const ElementT* const ndata, const InputT fracx, const InputT fracy)
-    {
-        auto x1 = cubicPolate( ndata[0], ndata[1], ndata[2], ndata[3], fracx );
-        auto x2 = cubicPolate( ndata[4], ndata[5], ndata[6], ndata[7], fracx );
-        auto x3 = cubicPolate( ndata[8], ndata[9], ndata[10], ndata[11], fracx );
-        auto x4 = cubicPolate( ndata[12], ndata[13], ndata[14], ndata[15], fracx );
-
-        return std::clamp(
-                    cubicPolate( x1, x2, x3, x4, fracy ), 
-                    static_cast<InputT>(std::numeric_limits<ElementT>::min()),
-                    static_cast<InputT>(std::numeric_limits<ElementT>::max()));
     }
 
     //  multiple standard deviations
