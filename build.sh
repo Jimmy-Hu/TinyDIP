52fc51f8502deaeabaf7432cc017dcc6ddf23e66
jimmyhu <60071jimmy@gmail.com>
2021-07-31 13:52:35 +0800

    Update build.sh
    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf build && mkdir build
 cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake ..
-cmake --build .
+cmake --build . --parallel --verbose
 cd .. && ./build/TinyDIP
