6f76f46762d317b6bfd839b69570588d6e08c085
jimmyhu <60071jimmy@gmail.com>
2021-08-15 22:20:55 +0800

    Update testing code with Boost test library
    Update testing code with Boost test library

--- main.cpp
+++ main.cpp
@@ -46,11 +46,13 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(image_elementwise_minus_test, T, test_types)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(image_elementwise_multiplies_test, T, test_types)
 {
+    std::size_t size_x = 10;
+    std::size_t size_y = 10;
     T initVal = 10;
     T multiplier = 2;
-    auto test = TinyDIP::Image<T>(10, 10, initVal);
-    test *= TinyDIP::Image<T>(10, 10, multiplier);
-    BOOST_TEST(test == TinyDIP::Image<T>(10, 10, initVal * multiplier));
+    auto test = TinyDIP::Image<T>(size_x, size_y, initVal);
+    test *= TinyDIP::Image<T>(size_x, size_y, multiplier);
+    BOOST_TEST(test == TinyDIP::Image<T>(size_x, size_y, initVal * multiplier));
 }
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(image_elementwise_divides_test, T, test_types)
