bb405ac830d6c9ce3997e1b4adc6a807731d19c0
jimmyhu <60071jimmy@gmail.com>
2021-08-21 21:43:40 +0800

    Update addLeadingZeros function definition
    Update addLeadingZeros function definition

--- main.cpp
+++ main.cpp
@@ -260,7 +260,7 @@ void addLeadingZeros(std::string input_path, std::string output_path)
         std::cout << filename << "\n";
         auto bmpimage = TinyDIP::bmp_read(filename.c_str(), true);
         char buff[100];
-        snprintf(buff, sizeof(buff), "%s%05d", output_path, i);
+        snprintf(buff, sizeof(buff), "%s%05d", output_path.c_str(), i);
         TinyDIP::bmp_write(buff, bmpimage);
     }
 }
