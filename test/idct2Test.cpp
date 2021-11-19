835108da9d08f231205e51c5f7496a40bf92b0f2
jimmyhu <60071jimmy@gmail.com>
2021-11-19 16:31:34 +0800

    Add idct2Test.cpp
    Add idct2Test.cpp

---
+++ test/idct2Test.cpp
@@ -0,0 +1,31 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void idct2Test();
+
+int main()
+{
+	idct2Test();
+	return 0;
+}
+
+void idct2Test()
+{
+	std::size_t N1 = 10, N2 = 10;
+	TinyDIP::Image<double> test_input(N1, N2);
+	for (std::size_t y = 1; y <= N2; y++)
+	{
+		for (std::size_t x = 1; x <= N1; x++)
+		{
+			test_input.at(y - 1, x - 1) = x * 10 + y;
+		}
+	}
+
+	test_input.print();
+
+	TinyDIP::idct2(TinyDIP::dct2(test_input)).print();
+
+	return;
+}
