012e32e1b898d59c4dde3e3816b4fe6d0dd803dd
jimmyhu <60071jimmy@gmail.com>
2021-11-19 15:50:19 +0800

    Add dct2Test.cpp
    Add dct2Test.cpp

---
+++ test/dct2Test.cpp
@@ -0,0 +1,29 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void dct2Test();
+
+int main()
+{
+	dct2Test();
+	return 0;
+}
+
+void dct2Test()
+{
+	std::size_t N1 = 10, N2 = 10;
+	TinyDIP::Image<double> test_input(N1, N2);
+	for (std::size_t y = 1; y <= N2; y++)
+	{
+		for (std::size_t x = 1; x <= N1; x++)
+		{
+			test_input.at(y - 1, x - 1) = x * 10 + y;
+		}
+	}
+
+	test_input.print();
+
+	TinyDIP::dct2(test_input).print();
+}
