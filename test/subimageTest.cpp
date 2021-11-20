6f0b74687661ae61505051f03852483fa86e4dff
jimmyhu <60071jimmy@gmail.com>
2021-11-20 20:34:03 +0800

    Add subimageTest.cpp
    Add subimageTest.cpp

---
+++ test/subimageTest.cpp
@@ -0,0 +1,27 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void subimageTest();
+
+int main()
+{
+    subimageTest();
+    return 0;
+}
+
+void subimageTest()
+{
+    TinyDIP::Image<GrayScale> image1(3, 3, 10);
+    image1.at(2, 2) = 1;
+    std::cout << "Width: " + std::to_string(image1.getWidth()) + "\n";
+    std::cout << "Height: " + std::to_string(image1.getHeight()) + "\n";
+    auto image2 = TinyDIP::copyResizeBicubic(image1, 12, 12);
+    std::cout << "Width: " + std::to_string(image2.getWidth()) + "\n";
+    std::cout << "Height: " + std::to_string(image2.getHeight()) + "\n";
+    image2.print();
+    image2 = TinyDIP::subimage(image2, 3, 3, 1, 1);
+    image2.print();
+    return;
+}
