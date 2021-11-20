a3015fc3f7e1d9a26ce977736cdda5c2b6c47669
jimmyhu <60071jimmy@gmail.com>
2021-11-20 21:47:39 +0800

    Update build.sh
    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 rm -rf build && mkdir build
 cmake -B ./build -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake -S .
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -S . -B ./build
 cmake --build ./build --parallel --verbose
 ./build/TinyDIP
