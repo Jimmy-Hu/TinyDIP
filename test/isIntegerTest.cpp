9cacb9ee1b1d9a41a2e64c559ca11403180556d5
jimmyhu <60071jimmy@gmail.com>
2021-11-20 20:40:06 +0800

    Add isIntegerTest.cpp
    Add isIntegerTest.cpp

---
+++ test/isIntegerTest.cpp
@@ -0,0 +1,18 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void isIntegerTest();
+
+int main()
+{
+	isIntegerTest();
+	return 0;
+}
+
+void isIntegerTest()
+{
+
+	return;
+}
