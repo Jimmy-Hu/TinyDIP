5d5f89acbc6ddbed08c9ade84f35ac251c45e003
jimmyhu <60071jimmy@gmail.com>
2021-11-24 09:00:16 +0800

    Add recursiveForeachTest.cpp
    Add recursiveForeachTest.cpp

---
+++ test/recursiveForeachTest.cpp
@@ -0,0 +1,24 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void recursiveForeachTest();
+
+int main()
+{
+	recursiveForeachTest();
+	return 0;
+}
+
+void recursiveForeachTest()
+{
+	auto print = [](const int& n) { std::cout << " " << n; };
+	std::vector<int> test_vector{ 1, 2, 3 };
+	TinyDIP::recursive_for_each<1>(test_vector, print);
+	std::cout << '\n';
+	std::vector<decltype(test_vector)> test_vector2{ test_vector, test_vector, test_vector };
+	TinyDIP::recursive_for_each<2>(test_vector2, print);
+
+	return;
+}
