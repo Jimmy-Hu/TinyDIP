5bb9dd3e1864bfd95fc745350aa9a008f9544ac4
jimmyhu <60071jimmy@gmail.com>
2021-11-24 14:21:20 +0800

    Add doubleImageIOTest.cpp
    Add doubleImageIOTest.cpp

---
+++ test/doubleImageIOTest.cpp
@@ -0,0 +1,41 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_io.h"
+#include "../image_operations.h"
+
+void doubleImageIOTest();
+
+int main()
+{
+    doubleImageIOTest();
+    return 0;
+}
+
+void doubleImageIOTest()
+{
+    std::size_t N1 = 10, N2 = 10;
+    TinyDIP::Image<double> test_input(N1, N2);
+    for (std::size_t y = 1; y <= N2; y++)
+    {
+        for (std::size_t x = 1; x <= N1; x++)
+        {
+            test_input.at(y - 1, x - 1) = x * 10 + y;
+        }
+    }
+
+    test_input.print();
+
+    auto dct_result = TinyDIP::dct2(test_input);
+    dct_result.print();
+    TinyDIP::double_image::write("test", dct_result);
+    
+    auto content_from_file = TinyDIP::double_image::read("test", false);
+    content_from_file.print();
+
+    std::cout << "Difference:\n";
+    TinyDIP::subtract(dct_result, content_from_file).print();
+
+    return;
+}
+
