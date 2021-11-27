1038c86851e6403481d7ce67e0511c49bba924d4
jimmyhu <60071jimmy@gmail.com>
2021-11-27 22:39:42 +0800

    Update dct2Test3.cpp
    Update dct2Test3.cpp

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -6,10 +6,20 @@
 #include "../image_operations.h"
 
 void each_image( std::string input_path, std::string output_path,
+	             std::string dictionary_path,
 	             std::size_t start_index = 50, std::size_t end_index = 100,
 	             std::size_t N1 = 8, std::size_t N2 = 8)
 {
 	auto input_img = TinyDIP::bmp_read(input_path.c_str(), false);
+	//***Load dictionary***
+	for (std::size_t i = start_index; i <= end_index; i++)
+	{
+		std::string fullpath = dictionary_path + "/" + std::to_string(i);
+		std::cout << "Dictionary path: " << fullpath << '\n';
+		auto input_dbmp = TinyDIP::double_image::read(fullpath.c_str(), false);
+
+	}
+
 }
 
 void dct2Test3( std::string arg1, std::string arg2,
