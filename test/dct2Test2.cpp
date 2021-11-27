9eb5a1ee6ea1ae010889a4b36a833accbd0914ff
jimmyhu <60071jimmy@gmail.com>
2021-11-27 21:32:40 +0800

    Update dct2Test2.cpp
    Update dct2Test2.cpp

--- test/dct2Test2.cpp
+++ test/dct2Test2.cpp
@@ -1,20 +1,27 @@
+#include <execution>
 #include "../base_types.h"
 #include "../basic_functions.h"
 #include "../image.h"
 #include "../image_io.h"
 #include "../image_operations.h"
 
-void dct2Test2(std::string arg1, std::string arg2)
+void dct2Test2(	std::string arg1, std::string arg2,
+				std::size_t N1 = 8, std::size_t N2 = 8)
 {
 	std::cout << "dct2Test2 program..." << '\n';
 	std::cout << arg1 << '\n';
 	std::cout << arg2 << '\n';
 	std::size_t start_index = 50, end_index = 100;
-	for (std::size_t i = start_index; i < end_index; i++)
+	for (std::size_t i = start_index; i <= end_index; i++)
 	{
 		std::string fullpath = arg1 + "/" + std::to_string(i);
 		std::cout << fullpath << '\n';
-		TinyDIP::bmp_read(fullpath.c_str(), false);
+		auto input_img = TinyDIP::bmp_read(fullpath.c_str(), false);
+		auto dct2_results = TinyDIP::recursive_transform<2>(
+			std::execution::par,
+			[](auto&& element) { return TinyDIP::dct2(element); },
+			TinyDIP::split(TinyDIP::getVplane(TinyDIP::rgb2hsv(input_img)), input_img.getWidth() / N1, input_img.getHeight() / N2)
+			);
 	}
 	return;
 }
