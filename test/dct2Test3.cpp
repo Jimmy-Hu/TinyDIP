cb944e40769df5ad34d9b63b6a429b539edfe0e3
jimmyhu <60071jimmy@gmail.com>
2021-11-27 22:26:31 +0800

    Update dct2Test3.cpp
    Update dct2Test3.cpp

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -5,13 +5,15 @@
 #include "../image_io.h"
 #include "../image_operations.h"
 
-void each_image( std::size_t start_index = 50, std::size_t end_index = 100,
+void each_image( std::string input_path, std::string output_path,
+	             std::size_t start_index = 50, std::size_t end_index = 100,
 	             std::size_t N1 = 8, std::size_t N2 = 8)
 {
-
+	auto input_img = TinyDIP::bmp_read(input_path.c_str(), false);
 }
 
-void dct2Test3( std::string arg1, std::string arg2)
+void dct2Test3( std::string arg1, std::string arg2,
+	            std::size_t N1 = 8, std::size_t N2 = 8)
 {
 
 }
