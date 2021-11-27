5c78f4b0ec5e5c21bce211506e2e41b628fd3569
jimmyhu <60071jimmy@gmail.com>
2021-11-27 21:19:08 +0800

    Update dct2Test2.cpp
    Update dct2Test2.cpp

--- test/dct2Test2.cpp
+++ test/dct2Test2.cpp
@@ -3,16 +3,18 @@
 #include "../image.h"
 #include "../image_operations.h"
 
-void dct2Test2(std::string arg1)
+void dct2Test2(std::string arg1, std::string arg2)
 {
 	std::cout << "dct2Test2 program..." << '\n';
 	std::cout << arg1 << '\n';
+	std::cout << arg2 << '\n';
 }
 
 int main(int argc, char* argv[])
 {
-	auto arg1 = std::string(argv[0]);
-	dct2Test2(arg1);
+	auto arg1 = std::string(argv[1]);
+	auto arg2 = std::string(argv[2]);
+	dct2Test2(arg1, arg2);
 	return 0;
 }
 
