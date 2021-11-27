b02807a5e8fd99bc0e4dadcb37bd44e191b878a1
jimmyhu <60071jimmy@gmail.com>
2021-11-27 20:33:49 +0800

    Remove subimage2 function definition
    Remove subimage2 function definition

--- image_operations.h
+++ image_operations.h
@@ -298,15 +298,6 @@ namespace TinyDIP
         return output;
     }
 
-    template<typename ElementT>
-    requires ((std::same_as<ElementT, RGB>) || (std::same_as<ElementT, HSV>))
-    constexpr static auto subimage2(const Image<ElementT>& input, std::size_t startx, std::size_t endx, std::size_t starty, std::size_t endy)
-    {
-        assert(startx <= endx);
-        assert(starty <= endy);
-        return apply_each(input, [startx, endx, starty, endy](auto&& planes) { return subimage2(planes, startx, endx, starty, endy); });
-    }
-
     /*  split function
     *   xsegments is a number for the block count in x axis
     *   ysegments is a number for the block count in y axis
