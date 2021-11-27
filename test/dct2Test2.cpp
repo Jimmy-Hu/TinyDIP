1fe9a108863fea199a994b1b1cf582eed193a937
jimmyhu <60071jimmy@gmail.com>
2021-11-27 19:59:40 +0800

    Add dct2Test2.cpp
    Add dct2Test2.cpp

---
+++ test/dct2Test2.cpp
@@ -0,0 +1,17 @@
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+void dct2Test2();
+
+int main()
+{
+	dct2Test2();
+	return 0;
+}
+
+void dct2Test2()
+{
+
+}
