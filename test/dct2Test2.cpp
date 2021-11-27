f8b1e6a135658eb56635e2d18704e98d589cbd09
jimmyhu <60071jimmy@gmail.com>
2021-11-27 21:52:10 +0800

    Update dct2Test2.cpp
    Update dct2Test2.cpp

--- test/dct2Test2.cpp
+++ test/dct2Test2.cpp
@@ -5,8 +5,20 @@
 #include "../image_io.h"
 #include "../image_operations.h"
 
+void each_image(std::string input_path, std::string output_path)
+{
+	auto input_img = TinyDIP::bmp_read(input_path.c_str(), false);
+	auto dct2_results = TinyDIP::recursive_transform<2>(
+		std::execution::par,
+		[](auto&& element) { return TinyDIP::dct2(element); },
+		TinyDIP::split(TinyDIP::getVplane(TinyDIP::rgb2hsv(input_img)), input_img.getWidth() / N1, input_img.getHeight() / N2)
+		);
+	auto dct2_combined = TinyDIP::concat(dct2_results);
+	TinyDIP::double_image::write(output_path.c_str(), dct2_combined);
+}
+
 void dct2Test2( std::string arg1, std::string arg2,
-                std::string arg3,
+                std::string arg3, std::string arg4,
                 std::size_t N1 = 8, std::size_t N2 = 8)
 {
 	std::cout << "dct2Test2 program..." << '\n';
@@ -17,16 +29,10 @@ void dct2Test2( std::string arg1, std::string arg2,
 	{
 		std::string fullpath = arg1 + "/" + std::to_string(i);
 		std::cout << fullpath << '\n';
-		auto input_img = TinyDIP::bmp_read(fullpath.c_str(), false);
-		auto dct2_results = TinyDIP::recursive_transform<2>(
-			std::execution::par,
-			[](auto&& element) { return TinyDIP::dct2(element); },
-			TinyDIP::split(TinyDIP::getVplane(TinyDIP::rgb2hsv(input_img)), input_img.getWidth() / N1, input_img.getHeight() / N2)
-			);
-		auto dct2_combined = TinyDIP::concat(dct2_results);
 		auto output_path = arg3 + "/" + std::to_string(i);
-		TinyDIP::double_image::write(output_path.c_str(), dct2_combined);
+		each_image(fullpath, output_path);
 	}
+
 	return;
 }
 
@@ -35,7 +41,8 @@ int main(int argc, char* argv[])
 	auto arg1 = std::string(argv[1]);
 	auto arg2 = std::string(argv[2]);
 	auto arg3 = std::string(argv[3]);
-	dct2Test2(arg1, arg2, arg3);
+	auto arg4 = std::string(argv[4]);
+	dct2Test2(arg1, arg2, arg3, arg4);
 	return 0;
 }
 
