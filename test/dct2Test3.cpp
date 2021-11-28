46299daeb309622ce68a6856c186ec0035ac56cb
jimmyhu <60071jimmy@gmail.com>
2021-11-28 08:07:43 +0800

    Update dct2Test3.cpp
    Update dct2Test3.cpp

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -12,7 +12,12 @@ constexpr static auto get_offset( const TinyDIP::Image<ElementT>& input,
 	                              const ElementT sigma)
 {
 	auto output = TinyDIP::Image(input.getWidth(), input.getHeight(), ElementT{});
-	auto weights = TinyDIP::recursive_transform<1>([&](auto&& element) { return 0; }, dictionary_x);
+	auto weights = TinyDIP::recursive_transform<1>(
+		[&](auto&& element) 
+		{ 
+			return TinyDIP::normalDistribution1D(ElementT{}, sigma);
+		}, dictionary_x);
+
 	return output;
 }
 
