d55c35bbb7e4e13c704824d9122f2d76e6bbc2cd
jimmyhu <60071jimmy@gmail.com>
2021-11-28 12:57:02 +0800

    Update dct2Test3.cpp
    Update dct2Test3.cpp

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <execution>
 #include "../base_types.h"
 #include "../basic_functions.h"
@@ -124,9 +125,14 @@ void dct2Test3( std::string input_folder, std::string output_folder,
 
 int main(int argc, char* argv[])
 {
+	auto start = std::chrono::system_clock::now();
 	auto arg1 = std::string(argv[1]);
 	auto arg2 = std::string(argv[2]);
 	auto arg3 = std::string(argv[3]);
 	dct2Test3(arg1, arg2, arg3);
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
 	return 0;
 }
