60139f7fb4d8eeb6002f03adff5306e27a98fdfc
jimmyhu <60071jimmy@gmail.com>
2021-12-08 21:26:03 +0800

    Update idct3DetailTest.cpp
    Update idct3DetailTest.cpp

--- test/idct3DetailTest.cpp
+++ test/idct3DetailTest.cpp
@@ -16,7 +16,12 @@ void print3(std::vector<TinyDIP::Image<ElementT>> input)
 
 int main()
 {
+	auto start = std::chrono::system_clock::now();
 	idct3DetailTest();
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
 	return 0;
 }
 
