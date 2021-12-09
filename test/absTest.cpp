cb90b65cd98a00c5849aebeee02734de036d4248
jimmyhu <60071jimmy@gmail.com>
2021-12-10 01:09:40 +0800

    Update absTest.cpp
    Update absTest.cpp

--- test/absTest.cpp
+++ test/absTest.cpp
@@ -3,18 +3,24 @@
 #include "../image.h"
 #include "../image_operations.h"
 
-void absTest();
+template<typename T>
+void absTest(T initVal)
+{
+	std::size_t sizeNum = 10;
+	auto test_img = TinyDIP::Image<T>(sizeNum, sizeNum, initVal);
+	TinyDIP::abs(test_img).print();
+	return;
+}
 
 int main()
 {
-	absTest();
+	auto start = std::chrono::system_clock::now();
+	absTest<int>(-10);
+	absTest<float>(-10.0);
+	absTest<double>(-10.0);
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
 	return 0;
-}
-
-void absTest()
-{
-	std::size_t sizeNum = 10;
-	auto test_img = TinyDIP::Image(sizeNum, sizeNum, -10);
-	TinyDIP::abs(test_img).print();
-	return;
 }
