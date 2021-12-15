53f50be91ed6e6e76bbc80948cfc08cf621f8b98
jimmyhu <60071jimmy@gmail.com>
2021-12-15 17:18:22 +0800

    Update getNFromVariadicTemplatesTest.cpp
    Update getNFromVariadicTemplatesTest.cpp

--- test/getNFromVariadicTemplatesTest.cpp
+++ test/getNFromVariadicTemplatesTest.cpp
@@ -6,16 +6,22 @@
 
 int main()
 {
-    int A = 1;
-    int B = 2;
-    int C = 3;
-    int D = 4;
-    int E = 5;
-    std::cout << TinyDIP::get_from_variadic_template<5>(A, B, C, D, E);
     static_assert(TinyDIP::get_from_variadic_template<1>(1, 2, 3, 4, 5) == 1);
     static_assert(TinyDIP::get_from_variadic_template<2>(1, 2, 3, 4, 5) == 2);
     static_assert(TinyDIP::get_from_variadic_template<3>(1, 2, 3, 4, 5) == 3);
     static_assert(TinyDIP::get_from_variadic_template<4>(1, 2, 3, 4, 5) == 4);
     static_assert(TinyDIP::get_from_variadic_template<5>(1, 2, 3, 4, 5) == 5);
+
+    int A = 1;
+    int B = 2;
+    int C = 3;
+    int D = 4;
+    int E = 5;
+    
+    assert(TinyDIP::get_from_variadic_template<1>(A, B, C, D, E) == A);
+    assert(TinyDIP::get_from_variadic_template<2>(A, B, C, D, E) == B);
+    assert(TinyDIP::get_from_variadic_template<3>(A, B, C, D, E) == C);
+    assert(TinyDIP::get_from_variadic_template<4>(A, B, C, D, E) == D);
+    assert(TinyDIP::get_from_variadic_template<5>(A, B, C, D, E) == E);
     return 0;
 }
