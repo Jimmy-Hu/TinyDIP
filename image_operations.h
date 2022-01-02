8b1d37e16893f05ccb164c7b877dc9cfff0e7d97
jimmyhu <60071jimmy@gmail.com>
2022-01-02 14:39:05 +0800

    Update idct3_detail function definition
    Update idct3_detail function definition

--- image_operations.h
+++ image_operations.h
@@ -782,9 +782,9 @@ namespace TinyDIP
                             auto l1 = (std::numbers::pi / (2 * N1) * (2 * x + 1) * static_cast<OutputT>(inner_x));
                             auto l2 = (std::numbers::pi / (2 * N2) * (2 * y + 1) * static_cast<OutputT>(inner_y));
                             auto l3 = (std::numbers::pi / (2 * static_cast<OutputT>(N3)) * (2 * static_cast<OutputT>(plane_index) + 1) * static_cast<OutputT>(inner_z));
-                            OutputT alpha1 = (inner_x == 0) ? (static_cast<OutputT>(1.0) / static_cast<OutputT>(std::sqrt(2))) : (static_cast<OutputT>(1.0));
-                            OutputT alpha2 = (inner_y == 0) ? (static_cast<OutputT>(1.0) / static_cast<OutputT>(std::sqrt(2))) : (static_cast<OutputT>(1.0));
-                            OutputT alpha3 = (inner_z == 0) ? (static_cast<OutputT>(1.0) / static_cast<OutputT>(std::sqrt(2))) : (static_cast<OutputT>(1.0));
+                            OutputT alpha1 = (inner_x == 0) ? (std::numbers::sqrt2_v<OutputT> / 2) : (static_cast<OutputT>(1.0));
+                            OutputT alpha2 = (inner_y == 0) ? (std::numbers::sqrt2_v<OutputT> / 2) : (static_cast<OutputT>(1.0));
+                            OutputT alpha3 = (inner_z == 0) ? (std::numbers::sqrt2_v<OutputT> / 2) : (static_cast<OutputT>(1.0));
                             sum += alpha1 * alpha2 * alpha3 * static_cast<OutputT>(plane.at(inner_x, inner_y)) *
                                 std::cos(l1) * std::cos(l2) * std::cos(l3);
                         }
