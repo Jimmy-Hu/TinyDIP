8d277bad1f5bc0c4f0cccfd84e3584b0cc70915a
jimmyhu <60071jimmy@gmail.com>
2022-01-15 13:52:23 +0800

    Add is_width_same function definition
    Add is_width_same function definition

--- image_operations.h
+++ image_operations.h
@@ -25,6 +25,12 @@ namespace TinyDIP
     }
 
     template<typename ElementT>
+    constexpr bool is_width_same(const Image<ElementT>& x, const Image<ElementT>& y, const Image<ElementT>& z)
+    {
+        return is_width_same(x, y) && is_width_same(y, z) && is_width_same(x, z);
+    }
+
+    template<typename ElementT>
     constexpr bool is_height_same(const Image<ElementT>& x, const Image<ElementT>& y)
     {
         return x.getHeight() == y.getHeight();
