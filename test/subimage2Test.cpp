ae0d3c2349a23ce09dafd6a4a88e1ad012aa62be
jimmyhu <60071jimmy@gmail.com>
2022-02-15 11:47:24 +0800

    Update subimage2Test.cpp
    Update subimage2Test.cpp

--- test/subimage2Test.cpp
+++ test/subimage2Test.cpp
@@ -22,9 +22,14 @@ void subimage2Test(const std::size_t sizex = 3, const std::size_t sizey = 3)
 
 int main()
 {
+    auto start = std::chrono::system_clock::now();
     subimage2Test<int>();
     subimage2Test<long>();
     subimage2Test<float>();
     subimage2Test<double>();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
     return 0;
 }
