6015c69ca9bcf1e830c0af2c3878ac6168468101
jimmyhu <60071jimmy@gmail.com>
2022-02-20 15:59:38 +0800

    Update idct2Test.cpp
    Update idct2Test.cpp

--- test/idct2Test.cpp
+++ test/idct2Test.cpp
@@ -25,6 +25,12 @@ void idct2Test(const std::size_t N1 = 10, const std::size_t N2 = 10)
 
 int main()
 {
+	auto start = std::chrono::system_clock::now();
+	idct2Test<float>();
 	idct2Test<double>();
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
 	return 0;
 }
