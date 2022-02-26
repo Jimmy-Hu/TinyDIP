66e8564af681c1421952652b341e13fc21f95124
jimmyhu <60071jimmy@gmail.com>
2022-02-26 16:59:30 +0800

    Update bicubicInterpolationTest.cpp
    Update bicubicInterpolationTest.cpp

--- test/bicubicInterpolationTest.cpp
+++ test/bicubicInterpolationTest.cpp
@@ -25,7 +25,12 @@ void bicubicInterpolationTest()
 
 int main()
 {
+    auto start = std::chrono::system_clock::now();
     bicubicInterpolationTest();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
     return 0;
 }
 
