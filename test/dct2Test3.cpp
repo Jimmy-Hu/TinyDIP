12628903f1c2da264982b8064ab0e16466f9bc77
jimmyhu <60071jimmy@gmail.com>
2022-03-03 23:59:14 +0800

    Update get_offset function definition
    Update get_offset function definition

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -35,7 +35,7 @@ constexpr static auto get_offset( ExPo execution_policy,
 			return TinyDIP::multiplies(input1, TinyDIP::Image(input1.getWidth(), input1.getHeight(), input2));
 		}, dictionary_y, weights);
 	//return TinyDIP::recursive_reduce(outputs, output, [](auto&& input1, auto&& input2) { return TinyDIP::plus(input1, input2); })
-	for (std::size_t i = 0; i < outputs.size(); i++)
+	for (std::size_t i = 0; i < outputs.size(); ++i)
 	{
 		output += outputs[i];
 	}
