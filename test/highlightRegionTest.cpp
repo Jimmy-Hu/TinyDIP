79c4deb5eea4e334b11395d26a5a95819248d429
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2022-04-16 11:10:31 +0800

    Add highlightRegionTest.cpp
    Add highlightRegionTest.cpp

---
+++ test/highlightRegionTest.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+template<typename T>
+void subimage2Test(const std::size_t sizex = 3, const std::size_t sizey = 3)
+{
+    TinyDIP::Image<T> image1(sizex, sizey, 10);
+    image1.at(2, 2) = 1;
+    std::cout << "Width: " + std::to_string(image1.getWidth()) + "\n";
+    std::cout << "Height: " + std::to_string(image1.getHeight()) + "\n";
+    auto image2 = TinyDIP::copyResizeBicubic<T>(image1, 12, 12);
+    std::cout << "Width: " + std::to_string(image2.getWidth()) + "\n";
+    std::cout << "Height: " + std::to_string(image2.getHeight()) + "\n";
+    image2.print();
+    image2 = TinyDIP::subimage2(image2, 0, 3, 0, 2);
+    image2.print();
+    return;
+}
+
+int main()
+{
+    auto start = std::chrono::system_clock::now();
+    subimage2Test<int>();
+    subimage2Test<long>();
+    subimage2Test<float>();
+    subimage2Test<double>();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return 0;
+}
