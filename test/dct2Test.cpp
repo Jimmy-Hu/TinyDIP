2b62d02e6454056d215f6ed15b4afd7f56760039
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2022-05-09 14:01:29 +0800

    Update dct2Test function implementation
    Update dct2Test function implementation

--- test/dct2Test.cpp
+++ test/dct2Test.cpp
@@ -16,7 +16,7 @@ void dct2Test()
 	std::cout << "dct2Test program..." << '\n';
 	std::size_t N1 = 10, N2 = 10;
 	TinyDIP::Image<double> test_input(N1, N2);
-	for (std::size_t y = 1; y <= N2; y++)
+	for (std::size_t y = 1; y <= N2; ++y)
 	{
 		for (std::size_t x = 1; x <= N1; x++)
 		{
