a0601bafe9060ae36f1680f9b9b734a1ed8a81f8
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2022-05-12 19:46:34 +0800

    Update recursive_count_if function implementation
    Update recursive_count_if function implementation

--- basic_functions.h
+++ basic_functions.h
@@ -288,12 +288,11 @@ namespace TinyDIP
 
     //  recursive_count_if implementation (the version with unwrap_level)
     template<std::size_t unwrap_level = 1, std::ranges::range T, class Pred>
+    requires(unwrap_level <= recursive_depth<T>())
     constexpr auto recursive_count_if(const T& input, const Pred& predicate)
     {
         if constexpr (unwrap_level > 1)
         {
-            static_assert(unwrap_level <= recursive_depth<T>(),
-                "unwrap level higher than recursion depth of input");
             return std::transform_reduce(std::ranges::cbegin(input), std::ranges::cend(input), std::size_t{}, std::plus<std::size_t>(), [predicate](auto&& element) {
                 return recursive_count_if<unwrap_level - 1>(element, predicate);
                 });
