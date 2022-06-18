d5fd9ce35a061258708bb82a6a5e2e5e5a101981
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2022-06-18 16:37:43 +0800

    Update recursiveFoldRightAllTest.cpp
    Update recursiveFoldRightAllTest.cpp

--- test/recursiveFoldRightAllTest.cpp
+++ test/recursiveFoldRightAllTest.cpp
@@ -20,6 +20,7 @@ void recursiveFoldRightAllTest()
     std::cout << "Play with test_vectors:\n\n";
     
     std::cout << "recursive_fold_right_all function test with vectors / std::plus<>(): \n";
+    auto recursive_fold_right_all_result1 = TinyDIP::recursive_fold_right_all(test_vectors, static_cast<int>(1), std::plus<>());
     
     return;
 }
