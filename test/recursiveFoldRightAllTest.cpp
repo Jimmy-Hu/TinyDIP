424b0fcb0fbed72ca438ff18888eb11d610306e9
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2022-09-21 16:08:30 +0800

    Update recursiveFoldRightAllTest.cpp
    Update recursiveFoldRightAllTest.cpp

--- test/recursiveFoldRightAllTest.cpp
+++ test/recursiveFoldRightAllTest.cpp
@@ -127,4 +127,6 @@ void recursiveFoldRightAllTestWithList(std::size_t times)
 
     std::cout << "recursive_fold_right_all function test with list / std::minus<>(): \n";
     std::cout << TinyDIP::recursive_fold_right_all(test_list1, static_cast<int>(2), std::minus<>()) << "\n\n";
+
+    std::cout << "recursive_fold_right_all function test with deque / std::multiplies<>(): \n";
 }
