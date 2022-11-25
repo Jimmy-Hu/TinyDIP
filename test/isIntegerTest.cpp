65da58ee4f8013c749cde6497a96df8327178f31
JimmyHu <60071jimmy@gmail.com>
2022-11-25 17:31:44 +0800

    Update isIntegerTest.cpp
    Update isIntegerTest.cpp

--- test/isIntegerTest.cpp
+++ test/isIntegerTest.cpp
@@ -32,6 +32,8 @@ void isIntegerTest()
 
 	assert(TinyDIP::is_integer(std::numeric_limits<double>::min()) == false);
 
+	assert(TinyDIP::is_integer(-std::numeric_limits<>));
+
 	float test_number1 = 1.2;
 	assert(TinyDIP::is_integer(test_number1) == false);
 	test_number1 = 1;
