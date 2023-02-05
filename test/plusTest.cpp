62a39a39876974868455fe5e92e7d871c91291cc
JimmyHu <60071jimmy@gmail.com>
2023-02-05 14:10:38 +0800

    Update plusTest.cpp
    Update plusTest.cpp

--- test/plusTest.cpp
+++ test/plusTest.cpp
@@ -9,8 +9,20 @@ template<class T>
 constexpr void plusTest(const std::size_t N1 = 10, const std::size_t N2 = 10)
 {
     auto image1 = TinyDIP::Image<T>(N1, N2, 1);
-    auto vector1 = std::vector<TinyDIP::Image<double>>();
+    auto vector1 = std::vector<decltype(image1)>();
     vector1.push_back(image1);
     TinyDIP::plus(vector1, vector1, vector1)[0].print();
 }
 
+int main()
+{
+	auto start = std::chrono::system_clock::now();
+	plusTest<int>();
+	plusTest<float>();
+	plusTest<double>();
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+	return 0;
+}
