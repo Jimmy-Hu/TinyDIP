0fc040eb57fa42ef81836494b3e8bb95b3d00315
JimmyHu <60071jimmy@gmail.com>
2023-04-04 18:37:06 +0800

    Update concatVerticalTest function implementation
    Update concatVerticalTest function implementation

--- test/concatVerticalTest.cpp
+++ test/concatVerticalTest.cpp
@@ -13,7 +13,8 @@ int main()
 
 void concatVerticalTest()
 {
-    TinyDIP::Image<GrayScale> image1(3, 3, 10);
+    TinyDIP::Image<GrayScale> image1(3, 3);
+    image1.setAllValue(10);
     image1.at(2, 2) = 1;
     std::cout << "Width: " + std::to_string(image1.getWidth()) + "\n";
     std::cout << "Height: " + std::to_string(image1.getHeight()) + "\n";
