4a1cfe21c046cb1f3a72a71a3c443894f63c1867
JimmyHu <60071jimmy@gmail.com>
2023-04-08 19:23:52 +0800

    Update concatVerticalTest.cpp
    Update concatVerticalTest.cpp

--- test/concatVerticalTest.cpp
+++ test/concatVerticalTest.cpp
@@ -11,6 +11,7 @@ int main()
 	concatVerticalTest();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 	return 0;
 }
 
