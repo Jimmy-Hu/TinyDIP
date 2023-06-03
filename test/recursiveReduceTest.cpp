b24e6345c6594548cac40ba80be1361f4825c35b
JimmyHu <60071jimmy@gmail.com>
2023-06-03 10:13:15 +0800

    Update recursiveReduceTest.cpp
    Update recursiveReduceTest.cpp

--- test/recursiveReduceTest.cpp
+++ test/recursiveReduceTest.cpp
@@ -8,9 +8,16 @@ void recursiveReduceTest();
 int main()
 {
     auto start = std::chrono::system_clock::now();
+    recursiveReduceTest();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
 	return 0;
+}
+
+
+void recursiveReduceTest()
+{
+    
 }
