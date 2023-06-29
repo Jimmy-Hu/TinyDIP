21fa39e3d92f7782c7ae3796884b2b1cd336d4ba
JimmyHu <60071jimmy@gmail.com>
2023-06-29 08:49:50 +0800

    Update recursiveAllOfTest.cpp
    Update recursiveAllOfTest.cpp

--- test/recursiveAllOfTest.cpp
+++ test/recursiveAllOfTest.cpp
@@ -15,6 +15,7 @@ int main()
 {
     auto start = std::chrono::system_clock::now();
 	recursiveAllOfTest<4>();
+    recursiveAllOfTest<5>();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
@@ -26,7 +27,7 @@ template<std::size_t dim>
 void recursiveAllOfTest()
 {
     auto test_vectors_1 = TinyDIP::n_dim_container_generator<dim, int>(1, 3);
-    
+
     std::cout << "Play with test_vectors_1:\n";
     
     assert(TinyDIP::recursive_all_of(test_vectors_1, [](int i) { return i % 2 == 0; }) == false);
