5e2defb39b923739dcba5a6ce2f0aae8dc18b0d2
JimmyHu <60071jimmy@gmail.com>
2023-08-05 14:54:31 +0800

    Update increaseIntensityTest.cpp
    Update increaseIntensityTest.cpp

--- test/increaseIntensityTest.cpp
+++ test/increaseIntensityTest.cpp
@@ -8,22 +8,38 @@ void increaseIntensityTest();
 
 int main()
 {
-	increaseIntensityTest();
-	return 0;
+    increaseIntensityTest();
+    return 0;
 }
 
 void increaseIntensityTest()
 {
-	auto bmp1 = TinyDIP::bmp_read("../../../InputImages/1", false);
-	auto hsv1 = TinyDIP::rgb2hsv(bmp1);
-	#pragma omp parallel for
-	for (int i = 0; i < 1; i++)
-	{
-		auto output = TinyDIP::hsv2rgb(TinyDIP::constructHSV(
-			TinyDIP::getHplane(hsv1),
-			TinyDIP::getSplane(hsv1),
-			TinyDIP::pixelwiseOperation([i](auto&& element) { return element + i; }, TinyDIP::getVplane(hsv1))
-		));
-		TinyDIP::bmp_write(std::to_string(i), output);
-	}
+    auto bmp1 = TinyDIP::bmp_read("../../../InputImages/1", false);
+    auto hsv1 = TinyDIP::rgb2hsv(bmp1);
+    if constexpr (false)
+    {
+        #pragma omp parallel for
+        for (int i = 0; i < 1; i++)
+        {
+            auto output = TinyDIP::hsv2rgb(TinyDIP::constructHSV(
+                TinyDIP::getHplane(hsv1),
+                TinyDIP::getSplane(hsv1),
+                TinyDIP::pixelwiseOperation([i](auto&& element) { return element + i; }, TinyDIP::getVplane(hsv1))
+            ));
+            TinyDIP::bmp_write(std::to_string(i), output);
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            auto output = TinyDIP::hsv2rgb(TinyDIP::constructHSV(
+                TinyDIP::getHplane(hsv1),
+                TinyDIP::getSplane(hsv1),
+                TinyDIP::pixelwiseOperation([i](auto&& element) { return element + i; }, TinyDIP::getVplane(hsv1))
+            ));
+            TinyDIP::bmp_write(std::to_string(i), output);
+        }
+    }
+    
 }
