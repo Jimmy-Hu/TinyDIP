d4d1345a682b3fe95694ac4674f98716241ff1c8
JimmyHu <60071jimmy@gmail.com>
2023-08-14 20:26:25 +0800

    Add recursiveFindIfTest.cpp
    Add recursiveFindIfTest.cpp

---
+++ test/recursiveFindIfTest.cpp
@@ -0,0 +1,22 @@
+/* Developed by Jimmy Hu */
+
+#include <execution>
+#include <stdlib.h>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_io.h"
+#include "../image_operations.h"
+
+
+
+int main()
+{
+    auto start = std::chrono::system_clock::now();
+    recursive_find_if_tests();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
+}
