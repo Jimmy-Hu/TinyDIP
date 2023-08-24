b8ab1cf9da108ded69673ecb1c4f7025df6e4cf5
JimmyHu <60071jimmy@gmail.com>
2023-08-24 18:07:45 +0800

    Update recursiveFindIfTest.cpp
    Update recursiveFindIfTest.cpp

--- test/recursiveFindIfTest.cpp
+++ test/recursiveFindIfTest.cpp
@@ -36,6 +36,7 @@ void recursive_find_if_tests()
     std::array<std::string, 3> word_array1 = {"foo", "foo", "foo"};
     assert(TinyDIP::recursive_find_if<1>(word_array1, [](auto&& i) { return i == "foo"; }));
     assert(TinyDIP::recursive_find_if<1>(word_array1, [](auto&& i) { return i == "bar"; }) == false);
+    assert(TinyDIP::recursive_find_if<2>(word_array1, [](auto&& i) { return i == 'a'; }) == false);
 
     //  Tests with std::deque of std::string
     std::deque<std::string> word_deque1 = {"foo", "foo", "foo", "bar"};
