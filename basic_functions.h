8a675dafed7cb168a521985adf4cedc1d07e1308
JimmyHu <60071jimmy@gmail.com>
2023-09-07 22:15:59 +0800

    Update recursive_all_of template function implementation
    Update recursive_all_of template function implementation

--- basic_functions.h
+++ basic_functions.h
@@ -215,7 +215,7 @@ namespace TinyDIP
     /*  recursive_all_of template function implementation with unwrap level
     */
     template<std::size_t unwrap_level, class T, class Proj = std::identity, class UnaryPredicate>
-    requires(unwrap_level <= recursive_depth<T>())
+    requires(unwrap_level <= recursive_depth<T>() && recursive_invocable<unwrap_level, Proj, T>)
     constexpr auto recursive_all_of(T&& value, UnaryPredicate&& p, Proj&& proj = {}) {
         if constexpr (unwrap_level > 0)
         {
