aae5d10d62258d1b8da9698bef1ed5dbd1bb9fca
JimmyHu <60071jimmy@gmail.com>
2023-09-24 13:42:56 +0800

    Add recursiveAnyOfTest.cpp
    Add recursiveAnyOfTest.cpp

---
+++ test/recursiveAnyOfTest.cpp
@@ -0,0 +1,9 @@
+/* Developed by Jimmy Hu */
+
+#include <execution>
+#include <stdlib.h>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_io.h"
+#include "../image_operations.h"
