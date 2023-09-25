487660d2c3372dac785458c0b60e24a19ea81653
JimmyHu <60071jimmy@gmail.com>
2023-09-25 13:45:16 +0800

    Update recursiveAnyOfTest.cpp
    Update recursiveAnyOfTest.cpp

--- test/recursiveAnyOfTest.cpp
+++ test/recursiveAnyOfTest.cpp
@@ -7,3 +7,16 @@
 #include "../image.h"
 #include "../image_io.h"
 #include "../image_operations.h"
+
+void recursive_any_of_tests()
+{
+    auto test_vectors_1 = TinyDIP::n_dim_container_generator<4, int, std::vector>(1, 3);
+    test_vectors_1[0][0][0][0] = 2;
+    assert(TinyDIP::recursive_any_of<4>(test_vectors_1, [](auto&& i) { return i % 2 == 0; }));
+
+    auto test_vectors_2 = TinyDIP::n_dim_container_generator<4, int, std::vector>(3, 3);
+    assert(TinyDIP::recursive_any_of<4>(test_vectors_2, [](auto&& i) { return i % 2 == 0; }) == false);
+    
+    
+    return;
+}
