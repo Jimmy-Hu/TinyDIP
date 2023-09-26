c05b7fc5b87498cbd725677d5189c79ea126d6fb
JimmyHu <60071jimmy@gmail.com>
2023-09-26 14:23:36 +0800

    Update recursiveAnyOfTest.cpp
    Update recursiveAnyOfTest.cpp

--- test/recursiveAnyOfTest.cpp
+++ test/recursiveAnyOfTest.cpp
@@ -20,3 +20,14 @@ void recursive_any_of_tests()
     
     return;
 }
+
+int main()
+{
+    auto start = std::chrono::system_clock::now();
+    recursive_any_of_tests();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
+}
