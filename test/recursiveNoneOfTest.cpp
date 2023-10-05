367d6922e3e02e83e4f346a52304cbc4e7a6e0ab
JimmyHu <60071jimmy@gmail.com>
2023-10-05 15:03:01 +0800

    Update recursiveNoneOfTest.cpp
    Update recursiveNoneOfTest.cpp

--- test/recursiveNoneOfTest.cpp
+++ test/recursiveNoneOfTest.cpp
@@ -8,11 +8,17 @@
 #include "../image_io.h"
 #include "../image_operations.h"
 
+void recursive_none_of_tests()
+{
+    
+
+    return;
+}
 
 int main()
 {
     auto start = std::chrono::system_clock::now();
-    
+    recursive_none_of_tests();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
