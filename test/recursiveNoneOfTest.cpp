b8ad9d1e149a17994eb4e6ab743931698e217420
JimmyHu <60071jimmy@gmail.com>
2023-10-07 15:22:14 +0800

    Update recursiveNoneOfTest.cpp
    Update recursiveNoneOfTest.cpp

--- test/recursiveNoneOfTest.cpp
+++ test/recursiveNoneOfTest.cpp
@@ -14,7 +14,10 @@ void recursive_none_of_tests()
     test_vectors_1[0][0][0][0] = 2;
     assert(TinyDIP::recursive_none_of<4>(test_vectors_1, [](auto&& i) { return i % 2 == 0; }) == false);
 
-
+    auto test_vectors_2 = TinyDIP::n_dim_container_generator<4, int, std::vector>(3, 3);
+    assert(TinyDIP::recursive_none_of<4>(test_vectors_2, [](auto&& i) { return i % 2 == 0; }));
+    
+    
     return;
 }
 
