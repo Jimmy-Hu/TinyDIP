8904fd90b33d8b41760dfc795659580d34141eae
JimmyHu <60071jimmy@gmail.com>
2023-11-01 10:34:04 +0800

    Update plusTest.cpp
    Update plusTest.cpp

--- test/plusTest.cpp
+++ test/plusTest.cpp
@@ -23,6 +23,7 @@ int main()
 	plusTest<int>();
 	plusTest<float>();
 	plusTest<double>();
+	plusTest<long double>();
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed_seconds = end - start;
 	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
