11af2884a3bc26c9ad09f04de6d4e2a371425a18
JimmyHu <60071jimmy@gmail.com>
2023-11-03 15:38:23 +0800

    Update plusTest.cpp
    Update plusTest.cpp

--- test/plusTest.cpp
+++ test/plusTest.cpp
@@ -9,26 +9,26 @@ template<class T>
 constexpr void plusTest(const std::size_t N1 = 10, const std::size_t N2 = 10)
 {
     auto image1 = TinyDIP::Image<T>(N1, N2);
-	image1.setAllValue(1);
+    image1.setAllValue(1);
     auto vector1 = std::vector<decltype(image1)>();
     vector1.push_back(image1);
-	assert(TinyDIP::plus(vector1, vector1, vector1)[0].count() == N1 * N2);
-	std::cout << "Element count: " << TinyDIP::plus(vector1, vector1, vector1)[0].count() << "\n";
-	auto image2 = TinyDIP::Image<T>(N1, N2);
-	image2.setAllValue(3);
+    assert(TinyDIP::plus(vector1, vector1, vector1)[0].count() == N1 * N2);
+    auto image2 = TinyDIP::Image<T>(N1, N2);
+    image2.setAllValue(3);
     assert(TinyDIP::plus(vector1, vector1, vector1)[0] == image2);
 }
 
 int main()
 {
-	auto start = std::chrono::system_clock::now();
-	plusTest<int>();
-	plusTest<float>();
-	plusTest<double>();
-	plusTest<long double>();
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return 0;
+    auto start = std::chrono::system_clock::now();
+    plusTest<int>();
+    plusTest<long>();
+    plusTest<float>();
+    plusTest<double>();
+    plusTest<long double>();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return 0;
 }
