b34eccd0cc4ffa6b8344fed9fdd1b16d8c846d5e
JimmyHu <60071jimmy@gmail.com>
2023-11-07 13:24:59 +0800

    Update sumTest.cpp
    Update sumTest.cpp

--- test/sumTest.cpp
+++ test/sumTest.cpp
@@ -13,15 +13,19 @@ void sum_test(int sizex, int sizey)
 {
     auto test_image = TinyDIP::Image<T>(sizex, sizey);
     assert(TinyDIP::sum(test_image) == 0);
+    test_image.setAllValue(1);
+    auto sum_result = std::reduce(std::ranges::cbegin(test_image.getImageData()), std::ranges::cend(test_image.getImageData()), 0, std::plus())
+    assert(TinyDIP::sum(test_image) == sum_result);
+    return;
 }
 
 int main()
 {
     auto start = std::chrono::system_clock::now();
-	sum_test<double>(10, 10);
+    sum_test<double>(10, 10);
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
