e37eccf00a12eaa77c5ff47dd9ec7fb1c5586895
JimmyHu <60071jimmy@gmail.com>
2023-11-27 14:48:06 +0800

    Update build.sh
    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ cmake -B ./build -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.c
 cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -S . -B ./build
 cmake --build ./build --parallel --verbose
 ./build/TinyDIP
+./build/recursiveTransformTest
