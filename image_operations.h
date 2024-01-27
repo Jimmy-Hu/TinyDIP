f57e65d51b5b0d9496e10705c0fc1446cc07cf0d
JimmyHu <60071jimmy@gmail.com>
2024-01-27 16:48:59 +0800

    Update gaussian_fisheye template function implementation
    Update gaussian_fisheye template function implementation

--- image_operations.h
+++ image_operations.h
@@ -1147,7 +1147,7 @@ namespace TinyDIP
                 FloatingType distance_x = x - static_cast<FloatingType>(input.getWidth()) / 2.0;
                 FloatingType distance_y = y - static_cast<FloatingType>(input.getHeight()) / 2.0;
                 FloatingType distance = std::sqrt(std::pow(distance_x, 2) + std::pow(distance_y, 2));
-                FloatingType angle = std::atan2(distance_y, distance_x);
+                FloatingType angle = std::atan2(static_cast<FloatingType>(distance_y), static_cast<FloatingType>(distance_x));
                 FloatingType weight = normalDistribution2D(std::fabs(distance_x), std::fabs(distance_y), D0) / normalDistribution2D(0.0, 0.0, D0);
                 FloatingType new_distance = distance * weight;
                 FloatingType new_distance_x = new_distance * std::cos(angle);
