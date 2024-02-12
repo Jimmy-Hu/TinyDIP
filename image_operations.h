ba1dc9ea51898a559c918b2e522e0fa87b55c5ec
JimmyHu <60071jimmy@gmail.com>
2024-02-12 17:53:30 +0800

    Update butterworth_fisheye template function implementation
    Update butterworth_fisheye template function implementation

--- image_operations.h
+++ image_operations.h
@@ -1118,7 +1118,7 @@ namespace TinyDIP
                 FloatingType distance_x = x - static_cast<FloatingType>(input.getWidth()) / 2.0;
                 FloatingType distance_y = y - static_cast<FloatingType>(input.getHeight()) / 2.0;
                 FloatingType distance = std::hypot(distance_x, distance_y);
-                ElementT angle = std::atan2(distance_y, distance_x);
+                FloatingType angle = std::atan2(distance_y, distance_x);
                 ElementT weight = 1 / std::pow((1 + std::pow(distance / D0, 2 * N)), 0.5);
                 ElementT new_distance = distance * weight;
                 ElementT new_distance_x = new_distance * std::cos(angle);
