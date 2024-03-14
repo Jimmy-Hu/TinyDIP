96ec4c0c8d11307f367df2923ac5131c49011e87
JimmyHu <60071jimmy@gmail.com>
2024-03-14 13:54:48 +0800

    Update transposeTest.cpp
    Update transposeTest.cpp

--- test/transposeTest.cpp
+++ test/transposeTest.cpp
@@ -3,3 +3,19 @@
 #include "../basic_functions.h"
 #include "../image.h"
 #include "../image_operations.h"
+
+
+template<class T>
+void manhattanDistanceTest()
+{
+    std::size_t N1 = 10, N2 = 10;
+    TinyDIP::Image<T> test_input(N1, N2);
+    for (std::size_t y = 1; y <= N2; y++)
+    {
+        for (std::size_t x = 1; x <= N1; x++)
+        {
+            test_input.at(y - 1, x - 1) = x * 10 + y + y & 1;
+        }
+    }
+    
+}
