54d60a890ae334d13477e48e9ef495af3177d5d7
JimmyHu <60071jimmy@gmail.com>
2024-03-17 14:06:22 +0800

    Update transposeTest.cpp
    Update transposeTest.cpp

--- test/transposeTest.cpp
+++ test/transposeTest.cpp
@@ -8,13 +8,13 @@
 template<class T>
 void transposeTest()
 {
-    std::size_t N1 = 10, N2 = 10;
+    std::size_t N1 = 2, N2 = 3;
     TinyDIP::Image<T> test_input(N1, N2);
-    for (std::size_t y = 1; y <= N2; y++)
+    for (std::size_t y = 1; y <= N2; ++y)
     {
-        for (std::size_t x = 1; x <= N1; x++)
+        for (std::size_t x = 1; x <= N1; ++x)
         {
-            test_input.at(y - 1, x - 1) = x * 10 + y + y & 1;
+            test_input.at(y - 1, x - 1) = x * 10 + y;
         }
     }
 
