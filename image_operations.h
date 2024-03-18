4561b78f41eec58e1fa4c16d1f0f45cf6007a715
JimmyHu <60071jimmy@gmail.com>
2024-03-18 14:22:26 +0800

    Update rotate_detail template function implementation
    Update rotate_detail template function implementation

--- image_operations.h
+++ image_operations.h
@@ -1193,7 +1193,20 @@ namespace TinyDIP
         FloatingType new_height = 2 *
             std::hypot(half_width, half_height) *
             std::abs(std::sin(std::atan2(half_height, half_width) + radians));
-        Image<ElementT> output(input.getWidth() + 1, input.getHeight() + 1);
+        Image<ElementT> output(input.getWidth(), input.getHeight());
+        //  if 90° rotation case
+        if(radians == std::numbers::pi_v<long double> / 2.0)
+        {
+            for (std::size_t y = 0; y < input.getHeight(); ++y)
+            {
+                for (std::size_t x = 0; x < input.getWidth(); ++x)
+                {
+                    output.at(y, input.getWidth() - x - 1) = 
+                        input.at(x, y);
+                }
+            }
+            return output;
+        }
         for (std::size_t y = 0; y < input.getHeight(); ++y)
         {
             for (std::size_t x = 0; x < input.getWidth(); ++x)
