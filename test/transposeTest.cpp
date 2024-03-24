0a1bee19be77d5d1e7e51081949f9c00793da7e9
JimmyHu <60071jimmy@gmail.com>
2024-03-24 14:01:55 +0800

    Update transposeTest template function implementation
    Update transposeTest template function implementation

--- test/transposeTest.cpp
+++ test/transposeTest.cpp
@@ -4,17 +4,17 @@
 #include "../image.h"
 #include "../image_operations.h"
 
-
+//  transposeTest template function implementation
 template<class T>
 void transposeTest()
 {
     std::size_t N1 = 2, N2 = 3;
     TinyDIP::Image<T> test_input(N1, N2);
-    for (std::size_t y = 1; y <= N2; ++y)
+    for (std::size_t y = 0; y < N2; ++y)
     {
-        for (std::size_t x = 1; x <= N1; ++x)
+        for (std::size_t x = 0; x < N1; ++x)
         {
-            test_input.at(y - 1, x - 1) = x * 10 + y;
+            test_input.at(x, y) = x * 10 + y;
         }
     }
 
