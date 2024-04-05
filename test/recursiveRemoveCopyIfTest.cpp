0944b10c6862132f5d15b36aea75167ec88c8099
JimmyHu <60071jimmy@gmail.com>
2024-04-05 12:11:03 +0800

    Add recursive_remove_copy_if_execution_policy_tests function implementation
    Add recursive_remove_copy_if_execution_policy_tests function implementation

--- test/recursiveRemoveCopyIfTest.cpp
+++ test/recursiveRemoveCopyIfTest.cpp
@@ -132,6 +132,114 @@ void recursive_remove_copy_if_tests()
 
 }
 
+//  recursive_remove_copy_if_execution_policy_tests function implementation
+void recursive_remove_copy_if_execution_policy_tests()
+{
+    //  std::vector<int> test case
+    std::vector<int> test_vector_1 = {
+        1, 2, 3, 4, 5, 6
+    };
+    std::vector<int> expected_result_1 = {
+        1, 3, 5
+    };
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<1>(std::execution::par, test_vector_1, [](auto&& x) { return (x % 2) == 0; }) ==
+        expected_result_1,
+        "std::vector<int> test case failed");
+    
+    //  std::vector<std::vector<int>> test case
+    std::vector<decltype(test_vector_1)> test_vector_2 = {
+        test_vector_1, test_vector_1, test_vector_1
+    };
+    std::vector<std::vector<int>> expected_result_2 = {
+        expected_result_1, expected_result_1, expected_result_1
+    };
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<2>(std::execution::par, test_vector_2, [](auto&& x) { return (x % 2) == 0; }) ==
+        expected_result_2,
+        "std::vector<std::vector<int>> test case failed");
+
+    //  std::vector<std::string> test case
+    std::vector<std::string> test_vector_3 = {
+        "1", "2", "3", "4", "5", "6"
+    };
+    std::vector<std::string> expected_result_3 = {
+        "2", "3", "4", "5", "6"
+    };
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<1>(std::execution::par, test_vector_3, [](auto&& x) { return (x == "1"); }) ==
+        expected_result_3,
+        "std::vector<std::string> test case failed");
+
+    //  std::vector<std::vector<std::string>> test case
+    std::vector<std::vector<std::string>> test_vector_4 = {
+        test_vector_3, test_vector_3, test_vector_3
+    };
+    std::vector<std::vector<std::string>> expected_result_4 = {
+        expected_result_3, expected_result_3, expected_result_3
+    };
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<2>(std::execution::par, test_vector_4, [](auto&& x) { return (x == "1"); }) ==
+        expected_result_4,
+        "std::vector<std::vector<std::string>> test case failed");
+
+    //  std::deque<int> test case
+    std::deque<int> test_deque_1;
+    test_deque_1.push_back(1);
+    test_deque_1.push_back(2);
+    test_deque_1.push_back(3);
+    test_deque_1.push_back(4);
+    test_deque_1.push_back(5);
+    test_deque_1.push_back(6);
+    std::deque<int> expected_result_5;
+    expected_result_5.push_back(2);
+    expected_result_5.push_back(3);
+    expected_result_5.push_back(4);
+    expected_result_5.push_back(5);
+    expected_result_5.push_back(6);
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<1>(std::execution::par, test_deque_1, [](auto&& x) { return (x == 1); }) ==
+        expected_result_5,
+        "std::deque<int> test case failed"
+    );
+
+    //  std::deque<std::deque<int>> test case
+    std::deque<decltype(test_deque_1)> test_deque_2;
+    test_deque_2.push_back(test_deque_1);
+    test_deque_2.push_back(test_deque_1);
+    test_deque_2.push_back(test_deque_1);
+    std::deque<decltype(expected_result_5)> expected_result_6;
+    expected_result_6.push_back(expected_result_5);
+    expected_result_6.push_back(expected_result_5);
+    expected_result_6.push_back(expected_result_5);
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<2>(std::execution::par, test_deque_2, [](auto&& x) { return (x == 1); }) ==
+        expected_result_6,
+        "std::deque<std::deque<int>> test case failed"
+    );
+
+    //  std::list<int> test case
+    std::list<int> test_list_1 = { 1, 2, 3, 4, 5, 6 };
+    std::list<int> expected_result_7 = {1, 3, 5};
+    M_Assert(
+        TinyDIP::recursive_remove_copy_if<1>(std::execution::par, test_list_1, [](int x) { return (x % 2) == 0; }) ==
+        expected_result_7,
+        "std::list<int> test case failed"
+    );
+
+    //  std::list<std::list<int>> test case
+    std::list<std::list<int>> test_list_2 = { test_list_1, test_list_1, test_list_1, test_list_1 };
+    std::list<std::list<int>> expected_result_8 = {
+        expected_result_7, expected_result_7, expected_result_7, expected_result_7
+    };
+    M_Assert(
+            TinyDIP::recursive_remove_copy_if<2>(std::execution::par, test_list_2, [](int x) { return (x % 2) == 0; }) ==
+            expected_result_8,
+            "std::list<std::list<int>> test case failed"
+        );
+
+}
+
 int main()
 {
     auto start = std::chrono::system_clock::now();
