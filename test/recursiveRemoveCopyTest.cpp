07467da4aba66fecd4c4395f9e80def9b7f942a1
JimmyHu <60071jimmy@gmail.com>
2024-04-08 10:52:13 +0800

    Add recursiveRemoveCopyTest.cpp
    Add recursiveRemoveCopyTest.cpp

---
+++ test/recursiveRemoveCopyTest.cpp
@@ -0,0 +1,12 @@
+/* Developed by Jimmy Hu */
+
+#include <cassert>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+
+int main()
+{
+    
+}
