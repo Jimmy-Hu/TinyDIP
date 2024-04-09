5d9aaf1ce95161c8f46b92611d4f45a809d9c83a
JimmyHu <60071jimmy@gmail.com>
2024-04-09 10:38:28 +0800

    Update recursiveRemoveCopyTest.cpp
    Update recursiveRemoveCopyTest.cpp

--- test/recursiveRemoveCopyTest.cpp
+++ test/recursiveRemoveCopyTest.cpp
@@ -8,5 +8,11 @@
 
 int main()
 {
+    auto start = std::chrono::system_clock::now();
     
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
 }
