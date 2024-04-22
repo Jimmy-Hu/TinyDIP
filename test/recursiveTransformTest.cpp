b1a247e8fc2324ac31f077a3178f8677e6af5172
JimmyHu <60071jimmy@gmail.com>
2024-04-22 11:33:42 +0800

    Update recursiveTransformTest.cpp
    Update recursiveTransformTest.cpp

--- test/recursiveTransformTest.cpp
+++ test/recursiveTransformTest.cpp
@@ -5,6 +5,25 @@
 #include "../image.h"
 #include "../image_operations.h"
 
+//  Copy from https://stackoverflow.com/a/37264642/6667035
+#ifndef NDEBUG
+#   define M_Assert(Expr, Msg) \
+    __M_Assert(#Expr, Expr, __FILE__, __LINE__, Msg)
+#else
+#   define M_Assert(Expr, Msg) ;
+#endif
+
+void __M_Assert(const char* expr_str, bool expr, const char* file, int line, const char* msg)
+{
+    if (!expr)
+    {
+        std::cerr << "Assert failed:\t" << msg << "\n"
+            << "Expected:\t" << expr_str << "\n"
+            << "Source:\t\t" << file << ", line " << line << "\n";
+        abort();
+    }
+}
+
 template<typename InputT>
 void recursiveTransformTest(InputT initialValue)
 {
