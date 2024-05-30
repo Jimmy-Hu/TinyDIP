2bf38a5c8e63d6a707241c10bce25d7bf600e77b
JimmyHu <60071jimmy@gmail.com>
2024-05-30 15:41:51 +0800

    Update dft2Test.cpp
    Update dft2Test.cpp

--- test/dft2Test.cpp
+++ test/dft2Test.cpp
@@ -7,3 +7,11 @@
 #include "../image_io.h"
 #include "../image_operations.h"
 
+int main()
+{
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
+}
