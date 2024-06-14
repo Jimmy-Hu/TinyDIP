a271b3166cf3905616b528c011982643543e4215
JimmyHu <60071jimmy@gmail.com>
2024-06-14 12:29:58 +0800

    Update concatTest.cpp
    Update concatTest.cpp

--- test/concatTest.cpp
+++ test/concatTest.cpp
@@ -9,7 +9,11 @@ int main()
 {
     auto start = std::chrono::system_clock::now();
     concatTest();
-    return 0;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
 }
 
 void concatTest()
