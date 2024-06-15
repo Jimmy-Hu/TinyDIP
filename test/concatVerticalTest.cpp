47093c7c8473da26706438e19ae6904fb0814f2d
JimmyHu <60071jimmy@gmail.com>
2024-06-15 08:54:04 +0800

    Add concatVerticalTest.cpp
    Add concatVerticalTest.cpp

--- test/concatVerticalTest.cpp
+++ test/concatVerticalTest.cpp
@@ -13,7 +13,7 @@ int main()
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return 0;
+	return EXIT_SUCCESS;
 }
 
 void concatVerticalTest()
