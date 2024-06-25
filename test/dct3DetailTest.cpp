d1440b0544a5bf79f6d3c7bcdc9e90a04338984b
JimmyHu <60071jimmy@gmail.com>
2024-06-25 14:40:10 +0800

    Update dct3DetailTest.cpp
    Update dct3DetailTest.cpp

--- test/dct3DetailTest.cpp
+++ test/dct3DetailTest.cpp
@@ -6,11 +6,11 @@
 template<typename ElementT>
 void print3(std::vector<TinyDIP::Image<ElementT>> input)
 {
-	for (std::size_t i = 0; i < input.size(); i++)
-	{
-		input[i].print();
-		std::cout << "*******************\n";
-	}
+    for (std::size_t i = 0; i < input.size(); i++)
+    {
+        input[i].print();
+        std::cout << "*******************\n";
+    }
 }
 
 template<typename T>
