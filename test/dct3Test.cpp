dc099a57d2f0a312df9696b83b76c5be7895cb0d
JimmyHu <60071jimmy@gmail.com>
2024-07-01 07:54:04 +0800

    Update dct3Test.cpp
    Update dct3Test.cpp

--- test/dct3Test.cpp
+++ test/dct3Test.cpp
@@ -15,22 +15,22 @@ void print3(std::vector<TinyDIP::Image<ElementT>>& input)
 
 void dct3Test(const std::size_t N1 = 10, const std::size_t N2 = 10, const std::size_t N3 = 10)
 {
-	std::vector<TinyDIP::Image<double>> test_input;
-	for (std::size_t z = 0; z < N3; z++)
-	{
-		test_input.push_back(TinyDIP::Image<double>(N1, N2));
-	}
-	for (std::size_t z = 1; z <= N3; z++)
-	{
-		for (std::size_t y = 1; y <= N2; y++)
-		{
-			for (std::size_t x = 1; x <= N1; x++)
-			{
-				test_input[z - 1].at(y - 1, x - 1) = x * 100 + y * 10 + z;
-			}
-		}
-	}
-	print3(test_input);
+    std::vector<TinyDIP::Image<double>> test_input;
+    for (std::size_t z = 0; z < N3; z++)
+    {
+        test_input.push_back(TinyDIP::Image<double>(N1, N2));
+    }
+    for (std::size_t z = 1; z <= N3; z++)
+    {
+        for (std::size_t y = 1; y <= N2; y++)
+        {
+            for (std::size_t x = 1; x <= N1; x++)
+            {
+                test_input[z - 1].at(y - 1, x - 1) = x * 100 + y * 10 + z;
+            }
+        }
+    }
+    print3(test_input);
 
 	auto test_output = TinyDIP::dct3(test_input);
 	print3(test_output);
