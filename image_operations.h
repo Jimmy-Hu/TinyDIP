cbc26bc305dd379efec38aa213dfb59ba22483a5
JimmyHu <60071jimmy@gmail.com>
2024-07-03 18:06:33 +0800

    Update conv2 template function implementation
    Update conv2 template function implementation

--- image_operations.h
+++ image_operations.h
@@ -117,21 +117,20 @@ namespace TinyDIP
         check_height_same(x, y);
     }
 
-    //  conv2 function implementation
+    //  conv2 template function implementation
     template<typename ElementT>
-    requires(std::floating_point<ElementT> || std::integral<ElementT>)
+    requires(std::floating_point<ElementT> || std::integral<ElementT> || is_complex<T>::value)
     constexpr auto conv2(const Image<ElementT>& x, const Image<ElementT>& y)
     {
         auto output = Image<ElementT>(x.getWidth() + y.getWidth() - 1, x.getHeight() + y.getHeight() - 1);
-        for (std::size_t x1 = 0; x1 < x.getWidth(); ++x1)
-        {
-            for (std::size_t y1 = 0; y1 < x.getHeight(); ++y1)
-            {
-                for (std::size_t x2 = 0; x2 < y.getWidth(); ++x2)
-                {
-                    for (std::size_t y2 = 0; y2 < y.getHeight(); ++y2)
-                    {
-                        output.at(x1 + x2, y1 + y2) = output.at(x1 + x2, y1 + y2) + x.at(x1, y1) * y.at(x2, y2);
+        for (std::size_t y1 = 0; y1 < x.getHeight(); ++y1) {
+            auto* x_row = &(x.at(0, y1));
+            for (std::size_t y2 = 0; y2 < y.getHeight(); ++y2) {
+                auto* y_row = &(y.at(0, y2));
+                auto* out_row = &(output.at(0, y1 + y2));
+                for (std::size_t x1 = 0; x1 < x.getWidth(); ++x1) {
+                    for (std::size_t x2 = 0; x2 < y.getWidth(); ++x2) {
+                        out_row[x1 + x2] += x_row[x1] * y_row[x2];
                     }
                 }
             }
