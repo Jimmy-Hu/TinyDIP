619773559bc9a321f56c40232182e912e89f6316
JimmyHu <60071jimmy@gmail.com>
2024-07-03 18:07:05 +0800

    Update conv2 template function implementation
    Update conv2 template function implementation

--- image_operations.h
+++ image_operations.h
@@ -140,8 +140,8 @@ namespace TinyDIP
 
     //  conv2 template function implementation
     template<typename ElementT, typename ElementT2>
-    requires ((std::same_as<ElementT, RGB>) || (std::same_as<ElementT, HSV>) &&
-              (std::floating_point<ElementT2> || std::integral<ElementT2>))
+    requires (((std::same_as<ElementT, RGB>) || (std::same_as<ElementT, RGB_DOUBLE>) || (std::same_as<ElementT, HSV>)) &&
+              (std::floating_point<ElementT2> || std::integral<ElementT2> || is_complex<ElementT2>::value))
     constexpr static auto conv2(const Image<ElementT>& input1, const Image<ElementT2>& input2)
     {
         return apply_each(input1, [&](auto&& planes) { return conv2(planes, input2); });
