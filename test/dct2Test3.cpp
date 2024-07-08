0948f5d09626246cdeafbdd8a215c346ef09d8b5
JimmyHu <60071jimmy@gmail.com>
2024-07-08 16:19:37 +0800

    Update dct2Test3 Template Function Implementation
    Update dct2Test3 Template Function Implementation

--- test/dct2Test3.cpp
+++ test/dct2Test3.cpp
@@ -117,35 +117,7 @@ void dct2Test3( const std::string& input_folder, const std::string& output_folde
 {
     std::cout << "dct2Test3 program..." << '\n';
     std::cout << "sigma = " << std::to_string(sigma) << '\n';
-    //***Load dictionary***
-    std::vector<TinyDIP::Image<double>> x, y;
-    for (std::size_t i = dic_start_index; i <= dic_end_index; ++i)
-    {
-        std::string fullpath = dictionary_path + "/" + std::to_string(i);
-        std::cout << "Dictionary path: " << fullpath << '\n';
-        auto input_dbmp = TinyDIP::double_image::read(fullpath.c_str(), false);
-        auto dct_block_x = TinyDIP::split(input_dbmp, input_dbmp.getWidth() / N1, input_dbmp.getHeight() / N2);
-        TinyDIP::recursive_for_each<2>(
-            std::execution::seq,
-            [&](auto&& element) 
-            {
-                x.push_back(element);
-            },
-            dct_block_x);
-
-        std::string fullpath_gt = dictionary_path + "/GT";
-        auto input_dbmp_gt = TinyDIP::double_image::read(fullpath_gt.c_str(), false);
-        auto dct_block_y = TinyDIP::split(input_dbmp_gt, input_dbmp_gt.getWidth() / N1, input_dbmp_gt.getHeight() / N2);
-        TinyDIP::recursive_for_each<2>(
-            std::execution::seq,
-            [&](auto&& element)
-            {
-                y.push_back(element);
-            },
-            dct_block_y);
-    }
-    auto xy_diff = TinyDIP::recursive_transform([&](auto&& element1, auto&& element2) { return TinyDIP::subtract(element2, element1); }, x, y);
-    std::cout << "x count: " << x.size() << "\txy_diff count: " << xy_diff.size() << '\n';
+    
     
     for (std::size_t i = start_index; i <= end_index; ++i)
     {
