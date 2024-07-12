71158d0d5ad32abebbcd858988f1abde0ab4b200
JimmyHu <60071jimmy@gmail.com>
2024-07-12 13:47:43 +0800

    Update imgaussfiltTest.cpp
    Update imgaussfiltTest.cpp

--- test/imgaussfiltTest.cpp
+++ test/imgaussfiltTest.cpp
@@ -11,4 +11,20 @@
 #include "../image_io.h"
 #include "../image_operations.h"
 
+void imgaussfiltTest(std::string_view input_image_path = "InputImages/1", std::string_view output_image_path = "OutputImages/imgaussfiltTest")
+{
+    auto input_img = TinyDIP::bmp_read(std::string(input_image_path).c_str(), false);
+    auto output_img = TinyDIP::imgaussfilt(input_img);
+    TinyDIP::bmp_write(std::string(output_image_path).c_str(), output_img);
+}
 
+int main(int argc, char* argv[])
+{
+    auto start = std::chrono::system_clock::now();
+    imgaussfiltTest();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
+}
