4f8cb8a3036a257526ce8f8a359bb6bf51a7b8f7
JimmyHu <60071jimmy@gmail.com>
2024-07-18 22:54:21 +0800

    Update imgaussfilt template function implementation
    Update imgaussfilt template function implementation

--- image_operations.h
+++ image_operations.h
@@ -1709,7 +1709,7 @@ namespace TinyDIP
         {
             return imgaussfilt(input, sigma, sigma, static_cast<int>(2 * std::ceil(2 * sigma) + 1), is_size_same);
         }
-        return imgaussfilt(input, sigma, sigma, filter_size, is_size_same);
+        return imgaussfilt(input, sigma, sigma, filter_size, filter_size, is_size_same);
     }
 
     //  imgaussfilt template function implementation
