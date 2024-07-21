d0c6fedae8430efc4651455eadfa728e822cd7e5
JimmyHu <60071jimmy@gmail.com>
2024-07-21 16:25:27 +0800

    Add conv3Test.cpp
    Add conv3Test.cpp

---
+++ test/conv3Test.cpp
@@ -0,0 +1,20 @@
+/* Developed by Jimmy Hu */
+
+#include <cassert>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_io.h"
+#include "../image_operations.h"
+
+int main()
+{
+    auto start = std::chrono::system_clock::now();
+    auto image1 = TinyDIP::ones<double>(2, 2, 2);
+    TinyDIP::conv3(image1, image1).print();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
+}
