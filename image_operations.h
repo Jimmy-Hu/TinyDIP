401654ba631ea0fc527da0e047756d9d7cced459
JimmyHu <60071jimmy@gmail.com>
2024-07-27 14:30:34 +0800

    Add generate_mirror_padding_image template function implementation (with Execution Policy)
    Add generate_mirror_padding_image template function implementation (with Execution Policy)

--- image_operations.h
+++ image_operations.h
@@ -1965,7 +1965,50 @@ namespace TinyDIP
         return generate_mirror_padding_image(std::execution::seq, input, width_expansion, height_expansion, default_value);
     }
 
-    
+    //  generate_mirror_padding_image template function implementation (with Execution Policy)
+    template<class ExecutionPolicy, typename ElementT>
+    requires(std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    constexpr static auto generate_mirror_padding_image(
+        ExecutionPolicy&& execution_policy, 
+        const Image<ElementT> input,
+        std::size_t width_expansion,
+        std::size_t height_expansion,
+        ElementT default_value = ElementT{})
+    {
+        auto output = generate_constant_padding_image(execution_policy, input, width_expansion, height_expansion, default_value);
+        auto flipped_vertical = flip_vertical(input);
+        output = paste2D(
+            execution_policy,
+            output,
+            subimage2(flipped_vertical, 0, flipped_vertical.getWidth() - 1, input.getHeight() - height_expansion - 1, flipped_vertical.getHeight() - 1),
+            width_expansion,
+            0,
+            default_value);
+        output = paste2D(
+            execution_policy,
+            output,
+            subimage2(flipped_vertical, 0, flipped_vertical.getWidth() - 1, 0, height_expansion),
+            width_expansion,
+            input.getHeight() + height_expansion - 1,
+            default_value);
+        auto flipped_horizontal = flip_horizontal(input);
+        output = paste2D(
+            execution_policy,
+            output,
+            subimage2(flipped_horizontal, input.getWidth() - width_expansion - 1, flipped_horizontal.getWidth() - 1, 0, flipped_horizontal.getHeight() - 1),
+            0,
+            height_expansion,
+            default_value);
+        output = paste2D(
+            execution_policy,
+            output,
+            subimage2(flipped_horizontal, 0, width_expansion, 0, flipped_horizontal.getHeight() - 1),
+            input.getWidth() + width_expansion - 1,
+            height_expansion,
+            default_value);
+        return output;
+    }
+
 
     //  computeFilterSizeFromSigma template function implementation
     template<typename ElementT>
