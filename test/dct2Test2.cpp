b7fed48877f26a0c22baa4f6f034e0c9884a76fa
JimmyHu <60071jimmy@gmail.com>
2024-08-07 11:45:54 +0800

    Add each_image_SuperResolution Function Implementation
    Add each_image_SuperResolution Function Implementation

--- test/dct2Test2.cpp
+++ test/dct2Test2.cpp
@@ -62,6 +62,42 @@ void dct2Test2( std::string arg1, std::string arg2,
     return;
 }
 
+//  each_image_SuperResolution Function Implementation
+/*
+* each_image_SuperResolution Function performs block based DCT on each channel of a RGB image
+*/
+void each_image_SuperResolution(
+    std::string input_path,
+    std::string output_path,
+    std::size_t N1 = 8,
+    std::size_t N2 = 8)
+{
+    auto input_img = TinyDIP::bmp_read(input_path.c_str(), false);
+
+    auto mod_x = std::fmod(static_cast<double>(input_img.getWidth()), static_cast<double>(N1));
+    auto mod_y = std::fmod(static_cast<double>(input_img.getHeight()), static_cast<double>(N2));
+
+    input_img = TinyDIP::subimage(
+        input_img,
+        input_img.getWidth() - mod_x, input_img.getHeight() - mod_y,
+        static_cast<double>(input_img.getWidth()) / 2.0, static_cast<double>(input_img.getHeight()) / 2.0
+    );
+    auto image_255 = TinyDIP::Image<double>(input_img.getWidth(), input_img.getHeight());
+    image_255.setAllValue(255);
+    auto dct2_R_results = TinyDIP::recursive_transform<2>(
+        std::execution::seq,
+        [](auto&& element) { return TinyDIP::dct2(element); },
+        TinyDIP::split(
+            TinyDIP::divides(
+                TinyDIP::im2double(TinyDIP::getRplane(input_img)),
+                image_255),
+            input_img.getWidth() / N1,
+            input_img.getHeight() / N2)
+    );
+    auto dct2_R_combined = TinyDIP::concat(dct2_R_results);
+    TinyDIP::double_image::write((output_path + std::string("_R")).c_str(), dct2_R_combined);
+}
+
 //  imageSuperResolutionExperiment Function Implementation
 void imageSuperResolutionExperiment(
     std::string input_high_res_img_path = "InputImages/HighRes",
