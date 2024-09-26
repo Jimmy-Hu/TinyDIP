bd0a0ce6f15f4b9150f2b8113b3b59446869b29b
JimmyHu <60071jimmy@gmail.com>
2024-09-26 10:35:38 +0800

    Update imbilatfilt template function implementation
    Update imbilatfilt template function implementation

--- image_operations.h
+++ image_operations.h
@@ -2875,7 +2875,7 @@ namespace TinyDIP
     //  imbilatfilt template function implementation
     template<typename ElementT, typename SigmaT = double>
     requires(std::floating_point<SigmaT> || std::integral<SigmaT>)
-    constexpr static auto imbilatfilt(const Image<ElementT>& input)
+    constexpr static auto imbilatfilt(const Image<ElementT>& input, SigmaT spatialSigma = 1, std::size_t NeighborhoodSize = 0)
     {
 
     }
