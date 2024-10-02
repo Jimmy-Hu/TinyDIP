025dbe65ba076224322213a9dde54586399f0436
JimmyHu <60071jimmy@gmail.com>
2024-10-02 20:08:32 +0800

    Update dft2Test.cpp
    Update dft2Test.cpp

--- test/dft2Test.cpp
+++ test/dft2Test.cpp
@@ -11,6 +11,9 @@ int main()
 {
     auto start = std::chrono::system_clock::now();
     TinyDIP::Image<double> I1(5, 5);
+    for(std::size_t y = 0; y < I1.getHeight(); ++y)
+    {
+    }
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
