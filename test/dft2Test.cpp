187a9fe5bda45afd583b849b30d55155a65bc5c0
JimmyHu <60071jimmy@gmail.com>
2024-10-08 10:55:38 +0800

    Update dft2Test.cpp
    Update dft2Test.cpp

--- test/dft2Test.cpp
+++ test/dft2Test.cpp
@@ -21,6 +21,7 @@ int main()
     I1.print();
     auto output = TinyDIP::dft2(I1);
     output.print();
+    auto inverse_output = TinyDIP::idft2(output);
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
