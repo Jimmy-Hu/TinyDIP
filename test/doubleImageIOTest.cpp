1473aabbad7afc09d04147613082ee070a9de0fe
JimmyHu <60071jimmy@gmail.com>
2024-10-15 12:58:45 +0800

    Update doubleImageIOTest.cpp
    Update doubleImageIOTest.cpp

--- test/doubleImageIOTest.cpp
+++ test/doubleImageIOTest.cpp
@@ -12,6 +12,7 @@ int main()
     doubleImageIOTest();
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     return EXIT_SUCCESS;
 }
 
