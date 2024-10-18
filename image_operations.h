b19c62964c840e80ef0b23abd362526e4a0f5d32
JimmyHu <60071jimmy@gmail.com>
2024-10-18 15:13:48 +0800

    Remove constexpr keyword
    Remove constexpr keyword

--- image_operations.h
+++ image_operations.h
@@ -344,7 +344,7 @@ namespace TinyDIP
     #endif
 
     //  rgb2hsv function implementation
-    constexpr static auto rgb2hsv(RGB input)
+    static auto rgb2hsv(RGB input)
     {
         HSV output{};
         std::uint8_t Red = input.channels[0], Green = input.channels[1], Blue = input.channels[2];
