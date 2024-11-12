4f2379c6761e624130e9726211891f939787f415
JimmyHu <60071jimmy@gmail.com>
2024-11-12 19:22:31 +0800

    Update gaussianFigure2DTest.cpp
    Update gaussianFigure2DTest.cpp

--- test/gaussianFigure2DTest.cpp
+++ test/gaussianFigure2DTest.cpp
@@ -14,7 +14,7 @@ int main()
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    if (elapsed_seconds.count() != 1)
     return EXIT_SUCCESS;
 }
 
