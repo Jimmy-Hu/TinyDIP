93aa068879d0f8904cada5987ef9914b2a3075e3
JimmyHu <60071jimmy@gmail.com>
2024-11-17 21:49:22 +0800

    Update gaussianFigure2DTest.cpp
    Update gaussianFigure2DTest.cpp

--- test/gaussianFigure2DTest.cpp
+++ test/gaussianFigure2DTest.cpp
@@ -19,6 +19,7 @@ int main()
         std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << " seconds.\n";
     }
     else
+    {
     return EXIT_SUCCESS;
 }
 
