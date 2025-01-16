28d3b7952cdd29bd4d93bf5ebe21808c82abc7d4
JimmyHu <60071jimmy@gmail.com>
2025-01-17 00:00:38 +0800

    Add apply_multichannel Template Function Implementation
    Add apply_multichannel Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -1617,6 +1617,18 @@ namespace TinyDIP
         return output;
     }
 
+    //  apply_multichannel Template Function Implementation (the version with execution policy)
+    template<std::size_t channel_count = 3, class ExecutionPolicy, class T, class Lambda, typename... Args>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)&&
+             ((std::same_as<T, RGB>) || (std::same_as<T, RGB_DOUBLE>) || (std::same_as<T, HSV>))
+    [[nodiscard]] constexpr static auto apply_multichannel(ExecutionPolicy&& execution_policy, const T& input, Lambda f, Args... args)
+    {
+        MultiChannel<decltype(std::invoke(f, input.channels[0], args...)), channel_count> output;
+        std::transform(execution_policy, std::ranges::cbegin(input.channels), std::ranges::cend(input.channels), std::ranges::begin(output.channels),
+            [&](auto&& input) { return std::invoke(f, input, args...); });
+        return output;
+    }
+
     //  abs Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto abs(const T& input)
