a0fbee2b678725938949fa6d798caec829690824
JimmyHu <60071jimmy@gmail.com>
2025-02-15 23:27:39 +0800

    Add sin Template Function Implementation
    Add sin Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -1759,6 +1759,21 @@ namespace TinyDIP
         }
     }
 
+    //  sin Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto sin(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::sin(_input); });
+        }
+        else
+        {
+            return std::sin(input);
+        }
+    }
+
 }
 
 #endif
