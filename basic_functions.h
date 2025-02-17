6812bfc0b9cc8bcbfe1ed0addcc0a3ac0df14fe4
JimmyHu <60071jimmy@gmail.com>
2025-02-17 13:38:35 +0800

    Add cos Template Function Implementation
    Add cos Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -1788,6 +1788,21 @@ namespace TinyDIP
         }
     }
 
+    //  cos Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto cos(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::cos(_input); });
+        }
+        else
+        {
+            return std::cos(input);
+        }
+    }
+
 }
 
 #endif
