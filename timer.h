1bb73dd125aa830a2aad3c61d893f34d4f7ecd7b
JimmyHu <60071jimmy@gmail.com>
2025-03-05 18:44:21 +0800

    Add timer.h
    Add timer.h

---
+++ timer.h
@@ -0,0 +1,41 @@
+/* Developed by Jimmy Hu */
+
+#ifndef Timer_H
+#define Timer_H
+
+#include <chrono>
+#include <iostream>
+
+namespace TinyDIP
+{
+    class Timer
+    {
+    private:
+        std::chrono::system_clock::time_point start, end;
+        std::chrono::duration<double> elapsed_seconds;
+        std::time_t end_time;
+    public:
+        Timer()
+        {
+            start = std::chrono::system_clock::now();
+        }
+
+        ~Timer()
+        {
+            end = std::chrono::system_clock::now();
+            elapsed_seconds = end - start;
+            end_time = std::chrono::system_clock::to_time_t(end);
+            if (elapsed_seconds.count() != 1)
+            {
+                std::print(std::cout, "Computation finished at {} elapsed time: {} seconds.\n", std::ctime(&end_time), elapsed_seconds.count());
+            }
+            else
+            {
+                std::print(std::cout, "Computation finished at {} elapsed time: {} second.\n", std::ctime(&end_time), elapsed_seconds.count());
+            }
+        }
+
+    };
+}
+
+#endif
