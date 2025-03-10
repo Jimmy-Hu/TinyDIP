97b1f2edb685c87ce2c522ea7ac8eb0572a116f0
JimmyHu <60071jimmy@gmail.com>
2025-03-10 22:43:24 +0800

    Add isinf Template Function Implementation (the version with execution policy)
    Add isinf Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -1947,6 +1947,21 @@ namespace TinyDIP
         }
     }
 
+    //  isinf Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto isinf(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::isinf(_input); });
+        }
+        else
+        {
+            return std::isinf(input);
+        }
+    }
+
     //  isnan Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isnan(const T& input)
