4d217785b5fcb1d627103614633b3971cf7b466f
JimmyHu <60071jimmy@gmail.com>
2025-03-10 22:03:53 +0800

    Add acos Template Function Implementation (the version with execution policy)
    Add acos Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -1860,6 +1860,21 @@ namespace TinyDIP
         }
     }
 
+    //  acos Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto acos(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::acos(_input); });
+        }
+        else
+        {
+            return std::acos(input);
+        }
+    }
+
     //  atan Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto atan(const T& input)
