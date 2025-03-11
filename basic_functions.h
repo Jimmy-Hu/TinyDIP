fe1042990808b98fc2aa5e0cf5ff5c52e5621d38
JimmyHu <60071jimmy@gmail.com>
2025-03-11 11:46:54 +0800

    Add sinh Template Function Implementation (the version with execution policy)
    Add sinh Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -1947,6 +1947,21 @@ namespace TinyDIP
         }
     }
 
+    //  sinh Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto sinh(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::sinh(_input); });
+        }
+        else
+        {
+            return std::sinh(input);
+        }
+    }
+
     //  isinf Template Function Implementation (the version with execution policy)
     template<class ExecutionPolicy, typename T>
     requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
