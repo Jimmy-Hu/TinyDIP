6ac18052c9d77d1279221f4ec255ca93db2dd5eb
JimmyHu <60071jimmy@gmail.com>
2025-03-14 13:58:08 +0800

    Add cosh Template Function Implementation (the version with execution policy)
    Add cosh Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -2034,6 +2034,21 @@ namespace TinyDIP
         }
     }
 
+    //  cosh Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto cosh(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::cosh(_input); });
+        }
+        else
+        {
+            return std::cosh(input);
+        }
+    }
+
     //  isinf Template Function Implementation (the version with execution policy)
     template<class ExecutionPolicy, typename T>
     requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
