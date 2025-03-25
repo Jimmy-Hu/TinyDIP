f3446350c054cf3f702f302f8e2a623a42493d25
JimmyHu <60071jimmy@gmail.com>
2025-03-25 16:18:54 +0800

    Add asinh Template Function Implementation (the version with execution policy)
    Add asinh Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -2108,6 +2108,21 @@ namespace TinyDIP
         }
     }
 
+    //  asinh Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto asinh(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::asinh(_input); });
+        }
+        else
+        {
+            return std::asinh(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
