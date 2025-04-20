f3a3e16bd0442dabfba6c93aefa9c8916efd87ac
JimmyHu <60071jimmy@gmail.com>
2025-04-20 18:31:16 +0800

    Add acosh Template Function Implementation (the version with execution policy)
    Add acosh Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -2161,6 +2161,21 @@ namespace TinyDIP
         }
     }
 
+    //  acosh Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto acosh(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return acosh(_input); });
+        }
+        else
+        {
+            return std::acosh(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
