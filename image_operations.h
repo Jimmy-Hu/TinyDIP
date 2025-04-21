949014b4347d21cf00eb5da1bd0cc84efdb44e81
JimmyHu <60071jimmy@gmail.com>
2025-04-21 14:23:49 +0800

    Add get_center_pixel template function implementation (with Execution Policy)
    Add get_center_pixel template function implementation (with Execution Policy)

--- image_operations.h
+++ image_operations.h
@@ -3311,6 +3311,27 @@ namespace TinyDIP
             );
     }
 
+    //  get_center_pixel template function implementation (with Execution Policy)
+    //  return center pixel value for N-dimensional image
+    template<typename ElementT, class ExecutionPolicy>
+    requires(std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    constexpr static auto get_center_pixel(
+        ExecutionPolicy&& execution_policy,
+        const Image<ElementT>& input
+    )
+    {
+        auto image_size = input.getSize();
+        auto center_location = image_size;
+        std::transform(
+            execution_policy,
+            std::ranges::cbegin(image_size),
+            std::ranges::cend(image_size),
+            std::ranges::begin(center_location),
+            [&](auto&& size) { return static_cast<double>(size) / 2.0; }
+        );
+        return input.at_without_boundary_check(center_location);
+    }
+
     //  draw_point template function implementation
     template<typename ElementT, std::size_t dimension = 2>
     constexpr static auto draw_point(
