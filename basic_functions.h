223de1bbcc34cdc20ca444550feed9e7047e257f
JimmyHu <60071jimmy@gmail.com>
2025-05-14 12:38:17 +0800

    Update cos Template Function Implementation (the version with execution policy)
    Update cos Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -1879,7 +1879,7 @@ namespace TinyDIP
     {
         if constexpr (Multichannel<T>)
         {
-            return apply_multichannel(execution_policy, input, [&](auto&& _input) {return std::cos(_input); });
+            return apply_multichannel(std::forward<ExecutionPolicy>(execution_policy), input, [&](auto&& _input) {return cos(std::forward<ExecutionPolicy>(execution_policy), _input); });
         }
         else
         {
