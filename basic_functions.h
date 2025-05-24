5d4828a1305ca2f768decc8fcbc6dfaba409e3a5
JimmyHu <60071jimmy@gmail.com>
2025-05-24 17:36:24 +0800

    Add erf Template Function Implementation
    Add erf Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2280,6 +2280,20 @@ namespace TinyDIP
         }
     }
 
+    //  erf Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto erf(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return erf(_input); });
+        }
+        else
+        {
+            return std::erf(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
