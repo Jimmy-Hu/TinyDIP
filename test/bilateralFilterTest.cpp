d74b3da2dc4af10e5a799ebfecb6472e9c325d35
JimmyHu <60071jimmy@gmail.com>
2025-05-29 15:46:25 +0800

    Update bilateralFilterTest.cpp
    Update bilateralFilterTest.cpp

--- test/bilateralFilterTest.cpp
+++ test/bilateralFilterTest.cpp
@@ -42,18 +42,44 @@ int main(int argc, char* argv[])
     }
     else
     {
-        std::string input_path = "../InputImages/1.bmp";
+        std::string input_path = "../InputImages/RainImages/S__55246868.bmp";
+
         auto input_img = TinyDIP::bmp_read(input_path.c_str(), true);
         input_img = TinyDIP::copyResizeBicubic(input_img, input_img.getWidth() * 3, input_img.getHeight() * 3);
-        auto output_img = TinyDIP::bilateral_filter(
-            std::execution::seq,
-            input_img,
+        std::cout << "Input image size: " << input_img.getWidth() << "x" << input_img.getHeight() << '\n';
+        auto double_image = TinyDIP::to_double(input_img);
+
+        auto progress_reporter = [](double progress) {
+            std::cout << "\rProcessing: "
+                << std::fixed << std::setprecision(1)
+                << (progress * 100.0) << "%" << std::flush;
+            };
+        auto bilateral_filter_output = TinyDIP::bilateral_filter(
+            std::execution::par,
+            double_image,
             20,
-            [](double input) { return TinyDIP::normalDistribution1D(input, 3.0); },
-            [](double input) { return TinyDIP::normalDistribution1D(input, 3.0); });
-        std::string output_path = "output_test";
-        std::cout << "Save output to " << output_path << '\n';
-        TinyDIP::bmp_write(output_path.c_str(), output_img);
+            [](auto&& input) { return TinyDIP::normalDistribution1D(input, 3.0); },
+            [](auto&& input) { return TinyDIP::normalDistribution1D(input, 3.0); },
+            TinyDIP::mirror,
+            progress_reporter);
+        auto difference_output = TinyDIP::increase_intensity(TinyDIP::difference(double_image, bilateral_filter_output), static_cast<TinyDIP::GrayScale>(50));
+        auto key_points = TinyDIP::SIFT_impl::get_potential_keypoint(TinyDIP::getVplane(TinyDIP::rgb2hsv(
+            difference_output
+        )));
+        std::cout << "Key points found: " << key_points.size() << '\n';
+        //TinyDIP::bmp_write("bilateral_filter_difference_output_keypoints", TinyDIP::draw_points(input_img, key_points, TinyDIP::RGB{ 255, 0, 0 }, 3));
+        if (TinyDIP::double_image::read("output_test_R", false) != TinyDIP::getRplane(bilateral_filter_output))
+        {
+            throw std::runtime_error("Computation result is different!");
+        }
+        if (TinyDIP::double_image::read("output_test_G", false) != TinyDIP::getGplane(bilateral_filter_output))
+        {
+            throw std::runtime_error("Computation result is different!");
+        }
+        if (TinyDIP::double_image::read("output_test_B", false) != TinyDIP::getBplane(bilateral_filter_output))
+        {
+            throw std::runtime_error("Computation result is different!");
+        }
         std::string difference_output_path = "difference_output";
         std::cout << "Save output to " << difference_output_path << '\n';
         TinyDIP::bmp_write(
