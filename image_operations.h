d97927c01a8010e97bbc54125528be8c402c4ae4
JimmyHu <60071jimmy@gmail.com>
2025-06-18 12:47:48 +0800

    Update randi template function implementation
    Update randi template function implementation

--- image_operations.h
+++ image_operations.h
@@ -282,7 +282,9 @@ namespace TinyDIP
     }
 
     // randi template function implementation
-    template<std::integral ElementT = int, std::same_as<std::size_t>... Size>
+    template<std::integral ElementT = int, class... Size>
+    requires((std::same_as<Size, std::size_t>&&...) or
+             (std::same_as<Size, int>&&...))
     inline auto randi(std::pair<ElementT, ElementT> min_and_max, Size... size)
     {
         return randi<ElementT>(std::mt19937{std::random_device{}()}, min_and_max, size...);
