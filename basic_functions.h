eb0fa5d0938071efcca432029bdb4f44e93b2143
JimmyHu <60071jimmy@gmail.com>
2025-07-06 10:14:00 +0800

    Add tgamma Template Function Implementation
    Add tgamma Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2371,6 +2371,20 @@ namespace TinyDIP
         }
     }
 
+    //  tgamma Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto tgamma(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return tgamma(_input); });
+        }
+        else
+        {
+            return std::tgamma(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
