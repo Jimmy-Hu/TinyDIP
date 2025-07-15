8865b09343dc0b3c6e2f3659c8f4570a570abe78
JimmyHu <60071jimmy@gmail.com>
2025-07-15 22:58:06 +0800

    Add  trunc Template Function Implementation (the version with execution policy)
    Add  trunc Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -2484,6 +2484,25 @@ namespace TinyDIP
         }
     }
 
+    //  trunc Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto trunc(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(
+                std::forward<ExecutionPolicy>(execution_policy),
+                input,
+                [&](auto&& _input) {return trunc(std::forward<ExecutionPolicy>(execution_policy), _input); }
+            );
+        }
+        else
+        {
+            return std::trunc(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
