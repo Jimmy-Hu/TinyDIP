2a883e9057dd3532f39e4ce953312bc0eb1d85fe
JimmyHu <60071jimmy@gmail.com>
2025-07-20 16:44:40 +0800

    Add ilogb Template Function Implementation
    Add ilogb Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2568,6 +2568,20 @@ namespace TinyDIP
         }
     }
 
+    //  ilogb Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto ilogb(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return ilogb(_input); });
+        }
+        else
+        {
+            return std::ilogb(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
