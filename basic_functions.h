47858732f3ecc8cab62762acb795c9ed4851c481
JimmyHu <60071jimmy@gmail.com>
2025-07-24 21:52:55 +0800

    Add fpclassify Template Function Implementation
    Add fpclassify Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2634,6 +2634,20 @@ namespace TinyDIP
         }
     }
 
+    //  fpclassify Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto fpclassify(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return fpclassify(_input); });
+        }
+        else
+        {
+            return std::fpclassify(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
