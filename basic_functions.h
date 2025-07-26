f6d6255ff433f4aa386604f20d3d13eaf02dfd0f
JimmyHu <60071jimmy@gmail.com>
2025-07-26 18:07:20 +0800

    Add isfinite Template Function Implementation
    Add isfinite Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2667,6 +2667,20 @@ namespace TinyDIP
         }
     }
 
+    //  isfinite Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto isfinite(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return isfinite(_input); });
+        }
+        else
+        {
+            return std::isfinite(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
