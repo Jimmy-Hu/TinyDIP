d785373538084bf81b0e4099779668568e4153c5
JimmyHu <60071jimmy@gmail.com>
2025-07-27 20:35:01 +0800

    Add isfinite Template Function Implementation (the version with execution policy)
    Add isfinite Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -2681,6 +2681,25 @@ namespace TinyDIP
         }
     }
 
+    //  isfinite Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto isfinite(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(
+                std::forward<ExecutionPolicy>(execution_policy),
+                input,
+                [&](auto&& _input) {return isfinite(std::forward<ExecutionPolicy>(execution_policy), _input); }
+            );
+        }
+        else
+        {
+            return std::isfinite(input);
+        }
+    }
+
     //  isinf Template Function Implementation
     template<typename T>
     [[nodiscard]] constexpr static auto isinf(const T& input)
