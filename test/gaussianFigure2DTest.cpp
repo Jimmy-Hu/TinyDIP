fcbff1e325f15af37d7e9e6a5d8f5cf691abd1a5
JimmyHu <60071jimmy@gmail.com>
2025-07-30 12:40:18 +0800

    Update gaussianFigure2DTest.cpp
    Update gaussianFigure2DTest.cpp

--- test/gaussianFigure2DTest.cpp
+++ test/gaussianFigure2DTest.cpp
@@ -3,25 +3,16 @@
 #include "../base_types.h"
 #include "../basic_functions.h"
 #include "../image.h"
+#include "../image_io.h"
 #include "../image_operations.h"
+#include "../timer.h"
 
 void gaussianFigure2DTest();
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
+    TinyDIP::Timer timer1;
     gaussianFigure2DTest();
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    if (elapsed_seconds.count() != 1)
-    {
-        std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << " seconds.\n";
-    }
-    else
-    {
-        std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << " second.\n";
-    }
     return EXIT_SUCCESS;
 }
 
@@ -29,5 +20,23 @@ void gaussianFigure2DTest()
 {
     auto gaussian_image = TinyDIP::gaussianFigure2D(10, 10, 5, 5, 1.0, 0.0, 1.0);
     gaussian_image.print();
+    auto gaussian_plane =
+        TinyDIP::gaussianFigure2D(
+            1024,
+            1024,
+            512,
+            512,
+            500.0,
+            500.0,
+            0.7
+        );
+    gaussian_plane = TinyDIP::multiplies(TinyDIP::normalize(gaussian_plane), 255.0);
+    TinyDIP::bmp_write("test_gaussian",
+        TinyDIP::constructRGB(
+            TinyDIP::im2uint8(gaussian_plane),
+            TinyDIP::im2uint8(gaussian_plane),
+            TinyDIP::im2uint8(gaussian_plane)
+        )
+    );
     return;
 }
