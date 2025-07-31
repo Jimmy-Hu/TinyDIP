ce4ec6e7731c467e54bae9aab0d670dbdbefed0d
JimmyHu <60071jimmy@gmail.com>
2025-07-31 10:19:32 +0800

    Update convnTest.cpp
    Update convnTest.cpp

--- test/convnTest.cpp
+++ test/convnTest.cpp
@@ -8,15 +8,12 @@
 #include "../image.h"
 #include "../image_io.h"
 #include "../image_operations.h"
+#include "../timer.h"
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
-    auto image1 = TinyDIP::ones<double>(std::size_t{2}, std::size_t{2}, std::size_t{2}, std::size_t{2});
+    TinyDIP::Timer timer1;
+    auto image1 = TinyDIP::ones<double>(std::size_t{ 2 }, std::size_t{ 3 }, std::size_t{ 2 }, std::size_t{ 2 });
     TinyDIP::convolution(image1, image1).print();
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
     return EXIT_SUCCESS;
 }
