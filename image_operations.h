1be2f3271f9f7235fef991ca65802dbfb9e36ea3
JimmyHu <60071jimmy@gmail.com>
2025-08-10 20:29:28 +0800

    Add all_zero template function implementation
    Add all_zero template function implementation

--- image_operations.h
+++ image_operations.h
@@ -46,6 +46,13 @@ namespace TinyDIP
         return all_of(input, [](const auto& element) { return element >= 0; });
     }
 
+    //  all_zero template function implementation
+    template<typename ElementT>
+    constexpr auto all_zero(const Image<ElementT>& input)
+    {
+        return all_of(input, [](const auto& element) { return element == 0; });
+    }
+
     template<typename ElementT>
     constexpr bool is_width_same(const Image<ElementT>& x, const Image<ElementT>& y)
     {
