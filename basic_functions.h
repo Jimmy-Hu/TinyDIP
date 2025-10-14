b709285977c027cf8a03e7f07cfec4efae6bf91a
JimmyHu <60071jimmy@gmail.com>
2025-10-14 16:15:35 +0800

    Add Formatter class implementation
    Add Formatter class implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2964,6 +2964,36 @@ namespace TinyDIP
         }
         return sum;
     }
+    //  Formatter class implementation
+    //  Copy from https://stackoverflow.com/a/12262626/6667035
+    class Formatter
+    {
+    public:
+        Formatter() {}
+        ~Formatter() {}
+
+        template <typename Type>
+        Formatter& operator << (const Type& value)
+        {
+            stream_ << value;
+            return *this;
+        }
+
+        std::string str() const { return stream_.str(); }
+        operator std::string() const { return stream_.str(); }
+
+        enum ConvertToString
+        {
+            to_str
+        };
+        std::string operator >> (ConvertToString) { return stream_.str(); }
+
+    private:
+        std::stringstream stream_;
+
+        Formatter(const Formatter&);
+        Formatter& operator = (Formatter&);
+    };
 }
 
 #endif
