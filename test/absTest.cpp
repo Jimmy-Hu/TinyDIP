8f3718a8973d03295e3a6351b6e0ec038455a248
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2025-10-22 22:48:33 +0800

    Update absTest.cpp
    Update absTest.cpp

--- test/absTest.cpp
+++ test/absTest.cpp
@@ -8,22 +8,40 @@
 template<typename T>
 void absTest(T initVal)
 {
-	std::size_t sizeNum = 10;
-	auto test_img = TinyDIP::Image<T>(sizeNum, sizeNum);
-	test_img.setAllValue(initVal);
-	TinyDIP::abs(test_img).print();
-	return;
+    std::size_t sizeNum = 10;
+    auto test_img = TinyDIP::Image<T>(sizeNum, sizeNum);
+    test_img.setAllValue(initVal);
+    TinyDIP::abs(test_img).print();
+    return;
+}
+
+template<typename T = TinyDIP::RGB_DOUBLE>
+void absRGB_DOUBLETest(T initVal)
+{
+    std::size_t sizeNum = 10;
+    auto test_img = TinyDIP::Image<T>(sizeNum, sizeNum);
+    test_img.setAllValue(initVal);
+    TinyDIP::abs(test_img).print();
+    TinyDIP::abs(std::execution::seq, test_img).print();
 }
 
 int main()
 {
-	auto start = std::chrono::system_clock::now();
-	absTest<int>(-10);
-	absTest<float>(-10.0);
-	absTest<double>(-10.0);
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-	std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return EXIT_SUCCESS;
+    auto start = std::chrono::system_clock::now();
+    absTest<int>(-10);
+    absTest<float>(-10.0);
+    absTest<double>(-10.0);
+    absRGB_DOUBLETest(TinyDIP::RGB_DOUBLE{ -1.0, -2.0, -3.0 });
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    if (elapsed_seconds.count() != 1)
+    {
+        std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << " seconds.\n";
+    }
+    else
+    {
+        std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << " second.\n";
+    }
+    return EXIT_SUCCESS;
 }
