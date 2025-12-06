9638c73831609eb6304720601d858fa97abc47e4
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2025-12-06 14:46:46 +0800

    Add log10 Template Function Implementation
    Add log10 Template Function Implementation

--- basic_functions.h
+++ basic_functions.h
@@ -2974,6 +2974,20 @@ namespace TinyDIP
         }
     }
 
+    //  log10 Template Function Implementation
+    template<typename T>
+    [[nodiscard]] constexpr static auto log10(const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(input, [&](auto&& _input) {return log10(_input); });
+        }
+        else
+        {
+            return std::log10(input);
+        }
+    }
+
 
     // sum_first_element Template Function Implementation
     template <typename FirstT, typename SecondT, class Function = std::plus<FirstT>>
