1f20265caf6d972bd3633c5a28a75b482702d3aa
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2025-12-11 17:02:39 +0800

    Add log2 Template Function Implementation (the version with execution policy)
    Add log2 Template Function Implementation (the version with execution policy)

--- basic_functions.h
+++ basic_functions.h
@@ -3054,6 +3054,24 @@ namespace TinyDIP
         }
     }
 
+    //  log2 Template Function Implementation (the version with execution policy)
+    template<class ExecutionPolicy, typename T>
+    requires (std::is_execution_policy_v<std::remove_cvref_t<ExecutionPolicy>>)
+    [[nodiscard]] constexpr static auto log2(ExecutionPolicy&& execution_policy, const T& input)
+    {
+        if constexpr (Multichannel<T>)
+        {
+            return apply_multichannel(
+                std::forward<ExecutionPolicy>(execution_policy),
+                input,
+                [&](auto&& _input) {return log2(std::forward<ExecutionPolicy>(execution_policy), _input); }
+            );
+        }
+        else
+        {
+            return std::log2(input);
+        }
+    }
 
     // sum_first_element Template Function Implementation
     template <typename FirstT, typename SecondT, class Function = std::plus<FirstT>>
