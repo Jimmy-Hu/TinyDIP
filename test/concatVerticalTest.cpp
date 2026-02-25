1545cd85ada419be4881b37f92e70e618a2812a1
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-02-25 13:39:40 +0800

    Update concatVerticalTest.cpp
    Update concatVerticalTest.cpp

--- test/concatVerticalTest.cpp
+++ test/concatVerticalTest.cpp
@@ -2,18 +2,15 @@
 #include "../basic_functions.h"
 #include "../image.h"
 #include "../image_operations.h"
+#include "../timer.h"
 
 void concatVerticalTest();
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
-	concatVerticalTest();
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return EXIT_SUCCESS;
+    TinyDIP::Timer timer1;
+    concatVerticalTest();
+    return EXIT_SUCCESS;
 }
 
 void concatVerticalTest()
