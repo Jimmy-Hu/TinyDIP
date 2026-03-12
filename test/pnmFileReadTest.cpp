ae9b6f55d457976bce236b39bfd138e8aeb014a9
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-12 19:59:00 +0800

    Update pnmFileReadTest.cpp
    Update pnmFileReadTest.cpp

--- test/pnmFileReadTest.cpp
+++ test/pnmFileReadTest.cpp
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include <execution>
+#include <filesystem>
 #include "../base_types.h"
 #include "../basic_functions.h"
 #include "../image.h"
@@ -11,16 +12,29 @@
 #include "../cube_operations.h"
 #include "../timer.h"
 
-void pnmFileReadTest(const std::filesystem::path& file_path)
+//  pnmFileReadTest function implementation
+void pnmFileReadTest(const std::filesystem::path& file_path, std::string_view output_path)
 {
     auto image_input = TinyDIP::pnm::read(file_path);
-    TinyDIP::bmp_write("read_from_pnm", image_input);
+    TinyDIP::bmp_write(std::string(output_path).c_str(), image_input);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     TinyDIP::Timer timer1;
-    const std::filesystem::path test_file = "ImgTest_0015.ppm";
-    pnmFileReadTest(test_file);
+    if (argc < 2)
+    {
+        
+        return EXIT_SUCCESS;
+    }
+    
+    std::filesystem::path input_path = std::string(argv[1]);
+    if (!std::filesystem::exists(input_path))
+    {
+        std::cerr << "File not found: " << input_path << "\n";
+        return EXIT_SUCCESS;
+    }
+    
+    pnmFileReadTest(input_path, "");
     return EXIT_SUCCESS;
 }
