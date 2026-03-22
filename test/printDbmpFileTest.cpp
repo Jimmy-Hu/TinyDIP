b9e9ece80823701f4a052116da9f1114c537b955
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-22 17:59:19 +0800

    Update printDbmpFileTest.cpp
    Update printDbmpFileTest.cpp

--- test/printDbmpFileTest.cpp
+++ test/printDbmpFileTest.cpp
@@ -29,29 +29,29 @@ static std::size_t string2size(std::string input)
 
 int main(int argc, char* argv[])
 {
-	auto start = std::chrono::system_clock::now();
-	std::cout << std::to_string(argc) << '\n';
-	if (argc == 2)
-	{
-		auto arg1 = std::string(argv[1]);
-		print_dbmp(arg1);
-	}
-	else if(argc == 6)
-	{
-		auto arg1 = std::string(argv[1]);
+    auto start = std::chrono::system_clock::now();
+    std::cout << std::to_string(argc) << '\n';
+    if (argc == 2)
+    {
+        auto arg1 = std::string(argv[1]);
+        print_dbmp(arg1);
+    }
+    else if(argc == 6)
+    {
+        auto arg1 = std::string(argv[1]);
         auto arg2 = string2size(std::string(argv[2]));
         auto arg3 = string2size(std::string(argv[3]));
         auto arg4 = string2size(std::string(argv[4]));
         auto arg5 = string2size(std::string(argv[5]));
-		print_dbmp(arg1, arg2, arg3, arg4, arg5);
-	}
+        print_dbmp(arg1, arg2, arg3, arg4, arg5);
+    }
     else
     {
 
     }
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-	std::cout << "Execution finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
-	return 0;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    std::cout << "Execution finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    return EXIT_SUCCESS;
 }
