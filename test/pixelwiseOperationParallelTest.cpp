d2b7b4ae7ed2b392f5d9b1949010215231b31f7f
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-23 15:07:22 +0800

    Rename function "pixelwiseOperation" to "pixelwise_transform"
    Rename function "pixelwiseOperation" to "pixelwise_transform"

--- test/pixelwiseOperationParallelTest.cpp
+++ test/pixelwiseOperationParallelTest.cpp
@@ -3,36 +3,36 @@
 #include "../image.h"
 #include "../image_operations.h"
 
-void pixelwiseOperationParallelTest();
+void pixelwise_transformParallelTest();
 
 int main()
 {
-	pixelwiseOperationParallelTest();
+	pixelwise_transformParallelTest();
 	return 0;
 }
 
-void pixelwiseOperationParallelTest()
+void pixelwise_transformParallelTest()
 {
 	for (size_t test_size = 0; test_size < 100; test_size++)
     {
 		auto img1 = TinyDIP::gaussianFigure2D(test_size, test_size, 5, 5, static_cast<double>(3));
         
-        auto output = TinyDIP::pixelwiseOperation
+        auto output = TinyDIP::pixelwise_transform
         (
             std::execution::par,
             [](auto&& pixel_in_img1)
             {
                 return 2 * pixel_in_img1;
             },
-            TinyDIP::pixelwiseOperation([](auto&& element) { return element; }, img1)
+            TinyDIP::pixelwise_transform([](auto&& element) { return element; }, img1)
         );
-        std::cout << TinyDIP::recursive_reduce(TinyDIP::subtract(output, TinyDIP::pixelwiseOperation
+        std::cout << TinyDIP::recursive_reduce(TinyDIP::subtract(output, TinyDIP::pixelwise_transform
         (
             [](auto&& pixel_in_img1)
             {
                 return 2 * pixel_in_img1;
             },
-            TinyDIP::pixelwiseOperation([](auto&& element) { return element; }, img1)
+            TinyDIP::pixelwise_transform([](auto&& element) { return element; }, img1)
         )).getImageData(), 0) << '\n';
     }
 	return;
