02bc36902e74b57af7dcca6ca3aa7aadfdae367b
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-24 18:27:01 +0800

    Update paste2DTest.cpp
    Update paste2DTest.cpp

--- test/paste2DTest.cpp
+++ test/paste2DTest.cpp
@@ -1,22 +1,56 @@
-/* Developed by Jimmy Hu */
+//  Developed by Jimmy Hu
 
+#include <algorithm>
 #include <cassert>
+#include <execution>
+#include <filesystem>
 #include "../base_types.h"
 #include "../basic_functions.h"
 #include "../image.h"
-#include "../image_io.h"
 #include "../image_operations.h"
+#include "../image_io.h"
+#include "../cube.h"
+#include "../cube_operations.h"
+#include "../timer.h"
 
-int main()
+//  paste2DTest function implementation
+void paste2DTest(const std::filesystem::path& file_path, std::string_view output_path)
 {
-    auto start = std::chrono::system_clock::now();
-    
-    auto image1 = TinyDIP::bmp_read("InputImages/1", false);
-    auto output = TinyDIP::paste2D(image1, image1, 100, 100);
-    TinyDIP::bmp_write("OutputImages/paste2D", output);
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
+    auto image_input = TinyDIP::bmp_read(file_path.string().c_str(), true);
+    TinyDIP::Image<TinyDIP::RGB> output_image(1920, 720);
+    output_image = TinyDIP::paste2D(output_image, image_input, 0, 0);
+    TinyDIP::bmp_write(std::string(output_path).c_str(), output_image);
+}
+
+int main(int argc, char* argv[])
+{
+    TinyDIP::Timer timer1;
+    if(argc == 3)
+    {
+        std::filesystem::path input_path = std::string(argv[1]);
+        if (!std::filesystem::exists(input_path))
+        {
+            std::cerr << "File / Path not found: " << input_path << '\n';
+            return EXIT_SUCCESS;
+        }
+        std::string target_ext = ".bmp";
+        if (std::filesystem::is_directory(input_path) && std::filesystem::is_directory(output_path))
+        {
+            for (const auto & entry : std::filesystem::directory_iterator(input_path))
+            {
+                if (entry.is_regular_file() && entry.path().extension() == target_ext)
+                {
+                    std::cout << "Processing " << entry.path() << '\n';
+                    paste2DTest(entry.path(), std::string(output_path / entry.path().stem()));
+                }
+            }
+        }
+    }
+    else
+    {
+        auto image1 = TinyDIP::bmp_read("InputImages/1", false);
+        auto output = TinyDIP::paste2D(image1, image1, 100, 100);
+        TinyDIP::bmp_write("OutputImages/paste2D", output);
+    }
     return EXIT_SUCCESS;
 }
