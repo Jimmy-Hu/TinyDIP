fce561547acc4cb13c2bb120ccce58c488cfc120
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-03-31 14:10:56 +0800

    Add subimage2Test2.cpp
    Add subimage2Test2.cpp

---
+++ test/subimage2Test2.cpp
@@ -0,0 +1,56 @@
+//  Developed by Jimmy Hu
+
+#include <algorithm>
+#include <cassert>
+#include <execution>
+#include <filesystem>
+#include "../base_types.h"
+#include "../basic_functions.h"
+#include "../image.h"
+#include "../image_operations.h"
+#include "../image_io.h"
+#include "../cube.h"
+#include "../cube_operations.h"
+#include "../timer.h"
+
+//  subimage2Test2 function implementation
+void subimage2Test2(const std::filesystem::path& file_path, std::string_view output_path)
+{
+    auto image_input = TinyDIP::bmp_read(file_path.string().c_str(), true);
+    output_image = TinyDIP::subimage2(image_input, 0, 1919, 0, 719);
+    TinyDIP::bmp_write(std::string(output_path).c_str(), output_image);
+}
+
+int main(int argc, char* argv[])
+{
+    TinyDIP::Timer timer1;
+    if(argc == 3)
+    {
+        std::filesystem::path input_path = std::string(argv[1]);
+        std::filesystem::path output_path = std::string(argv[2]);
+        if (!std::filesystem::exists(input_path))
+        {
+            std::cerr << "File / Path not found: " << input_path << '\n';
+            return EXIT_SUCCESS;
+        }
+        std::string target_ext = ".bmp";
+        if (std::filesystem::is_directory(input_path) && std::filesystem::is_directory(output_path))
+        {
+            for (const auto & entry : std::filesystem::directory_iterator(input_path))
+            {
+                if (entry.is_regular_file() && entry.path().extension() == target_ext)
+                {
+                    std::cout << "Processing " << entry.path() << '\n';
+                    paste2DTest(entry.path(), std::string(output_path / entry.path().stem()));
+                }
+            }
+        }
+    }
+    else
+    {
+        auto image1 = TinyDIP::bmp_read("InputImages/1", false);
+        auto output = TinyDIP::paste2D(image1, image1, 100, 100);
+        TinyDIP::bmp_write("OutputImages/subimage2Test2", output);
+    }
+    return EXIT_SUCCESS;
+}
