36b4e6b53f4bffa6fedfb27667c1d53de22225ec
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-04-03 17:36:27 +0800

    Update recursiveTransformTypeTest.cpp
    Update recursiveTransformTypeTest.cpp

--- test/recursiveTransformTypeTest.cpp
+++ test/recursiveTransformTypeTest.cpp
@@ -15,63 +15,65 @@
 #include "../timer.h"
 
 // Define a type alias to capture the container, function, expected type, and the actual resulting type.
-template <typename Container, typename Function, typename Expected>
+// Notice that the Function parameter now precedes the Container parameter in the decltype evaluation 
+// to match the updated TinyDIP::recursive_transform signature.
+template <std::size_t unwrap_level, typename Container, typename Function, typename Expected>
 using return_type_test_type = std::tuple<
     Container,
     Function,
     Expected,
-    decltype(TinyDIP::recursive_transform(std::execution::par, std::declval<Container>(), std::declval<Function>()))
+    decltype(TinyDIP::recursive_transform<unwrap_level>(std::execution::par, std::declval<Function>(), std::declval<Container>()))
 >;
 
 // The collection of types to test.
 using return_type_test_types = std::tuple<
     //                    Container   Function                Result
 
-    // Non-range.
+    // Non-range. (unwrap_level is 0 for all scalar types)
     //  template
     //  Replace "\n//T1 replace finished.\r\n\r\n" into "\n"
     /*
-    return_type_test_type<T1,        T2 (*)(T1),        T2>,
-    return_type_test_type<T1,        T2 (*)(T1 const&), T2>,
-    return_type_test_type<T1 const,  T2 (*)(T1 const&), T2>,
-    return_type_test_type<T1&,       T2 (*)(T1 const&), T2>,
-    return_type_test_type<T1 const&, T2 (*)(T1 const&), T2>,
-    return_type_test_type<T1&&,      T2 (*)(T1 const&), T2>,
+    return_type_test_type<0, T1,        T2 (*)(T1),        T2>,
+    return_type_test_type<0, T1,        T2 (*)(T1 const&), T2>,
+    return_type_test_type<0, T1 const,  T2 (*)(T1 const&), T2>,
+    return_type_test_type<0, T1&,       T2 (*)(T1 const&), T2>,
+    return_type_test_type<0, T1 const&, T2 (*)(T1 const&), T2>,
+    return_type_test_type<0, T1&&,      T2 (*)(T1 const&), T2>,
     */
-    return_type_test_type<char,        char (*)(char),        char>,
-    return_type_test_type<char,        char (*)(char const&), char>,
-    return_type_test_type<char const,  char (*)(char const&), char>,
-    return_type_test_type<char&,       char (*)(char const&), char>,
-    return_type_test_type<char const&, char (*)(char const&), char>,
-    return_type_test_type<char&&,      char (*)(char const&), char>,
+    return_type_test_type<0, char,        char (*)(char),        char>,
+    return_type_test_type<0, char,        char (*)(char const&), char>,
+    return_type_test_type<0, char const,  char (*)(char const&), char>,
+    return_type_test_type<0, char&,       char (*)(char const&), char>,
+    return_type_test_type<0, char const&, char (*)(char const&), char>,
+    return_type_test_type<0, char&&,      char (*)(char const&), char>,
 
-    return_type_test_type<int,        char (*)(int),        char>,
-    return_type_test_type<int,        char (*)(int const&), char>,
-    return_type_test_type<int const,  char (*)(int const&), char>,
-    return_type_test_type<int&,       char (*)(int const&), char>,
-    return_type_test_type<int const&, char (*)(int const&), char>,
-    return_type_test_type<int&&,      char (*)(int const&), char>,
+    return_type_test_type<0, int,        char (*)(int),        char>,
+    return_type_test_type<0, int,        char (*)(int const&), char>,
+    return_type_test_type<0, int const,  char (*)(int const&), char>,
+    return_type_test_type<0, int&,       char (*)(int const&), char>,
+    return_type_test_type<0, int const&, char (*)(int const&), char>,
+    return_type_test_type<0, int&&,      char (*)(int const&), char>,
 
-    return_type_test_type<short,        char (*)(short),        char>,
-    return_type_test_type<short,        char (*)(short const&), char>,
-    return_type_test_type<short const,  char (*)(short const&), char>,
-    return_type_test_type<short&,       char (*)(short const&), char>,
-    return_type_test_type<short const&, char (*)(short const&), char>,
-    return_type_test_type<short&&,      char (*)(short const&), char>,
+    return_type_test_type<0, short,        char (*)(short),        char>,
+    return_type_test_type<0, short,        char (*)(short const&), char>,
+    return_type_test_type<0, short const,  char (*)(short const&), char>,
+    return_type_test_type<0, short&,       char (*)(short const&), char>,
+    return_type_test_type<0, short const&, char (*)(short const&), char>,
+    return_type_test_type<0, short&&,      char (*)(short const&), char>,
 
-    return_type_test_type<long,        char (*)(long),        char>,
-    return_type_test_type<long,        char (*)(long const&), char>,
-    return_type_test_type<long const,  char (*)(long const&), char>,
-    return_type_test_type<long&,       char (*)(long const&), char>,
-    return_type_test_type<long const&, char (*)(long const&), char>,
-    return_type_test_type<long&&,      char (*)(long const&), char>,
+    return_type_test_type<0, long,        char (*)(long),        char>,
+    return_type_test_type<0, long,        char (*)(long const&), char>,
+    return_type_test_type<0, long const,  char (*)(long const&), char>,
+    return_type_test_type<0, long&,       char (*)(long const&), char>,
+    return_type_test_type<0, long const&, char (*)(long const&), char>,
+    return_type_test_type<0, long&&,      char (*)(long const&), char>,
 
-    return_type_test_type<long long int,        char (*)(long long int),        char>,
-    return_type_test_type<long long int,        char (*)(long long int const&), char>,
-    return_type_test_type<long long int const,  char (*)(long long int const&), char>,
-    return_type_test_type<long long int&,       char (*)(long long int const&), char>,
-    return_type_test_type<long long int const&, char (*)(long long int const&), char>,
-    return_type_test_type<long long int&&,      char (*)(long long int const&), char>
+    return_type_test_type<0, long long int,        char (*)(long long int),        char>,
+    return_type_test_type<0, long long int,        char (*)(long long int const&), char>,
+    return_type_test_type<0, long long int const,  char (*)(long long int const&), char>,
+    return_type_test_type<0, long long int&,       char (*)(long long int const&), char>,
+    return_type_test_type<0, long long int const&, char (*)(long long int const&), char>,
+    return_type_test_type<0, long long int&&,      char (*)(long long int const&), char>
 >;
 
 // -----------------------------------------------------------------------------
@@ -133,7 +135,8 @@ struct AddTwentySeven
 void test_empty_1d_int_vector()
 {
     const std::vector<int> input{};
-    const auto result = TinyDIP::recursive_transform(std::execution::par, input, ReturnMinusOne{});
+    // Vector depth is 1, so unwrap_level is 1. Passed function is now the second argument.
+    const auto result = TinyDIP::recursive_transform<1>(std::execution::par, ReturnMinusOne{}, input);
 
     // Output and validation
     if (result.empty())
@@ -152,7 +155,8 @@ void test_empty_1d_int_vector()
 void test_simple_int()
 {
     const auto input = 42;
-    const auto result = TinyDIP::recursive_transform(std::execution::par, input, AddTwentySeven{});
+    // Scalar depth is 0, so unwrap_level is 0. Passed function is now the second argument.
+    const auto result = TinyDIP::recursive_transform<0>(std::execution::par, AddTwentySeven{}, input);
 
     // Output and validation
     if (result == 69)
