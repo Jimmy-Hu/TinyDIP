5f0f7e94317f872a111f2c6991cf87a40f92fcec
jimmyhu <60071jimmy@gmail.com>
2026-04-05 03:42:13 +0800

    Update max template function implementation
    Update max template function implementation

--- image_operations.h
+++ image_operations.h
@@ -3617,7 +3617,7 @@ namespace TinyDIP
     requires ((std::same_as<ElementT, RGB>) || (std::same_as<ElementT, RGB_DOUBLE>) || (std::same_as<ElementT, HSV>))
     constexpr static auto max(const Image<ElementT>& input)
     {
-        return apply_each_single_output(input, [&](auto&& each_plane) { return max(each_plane); });
+        return apply_each_single_output(3, input, [&](auto&& each_plane) { return max(each_plane); });
     }
 
     //  minmax template function implementation
