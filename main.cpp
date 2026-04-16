15283ed49bb307e224ed6302b0aafb6e7803dc06
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-04-16 11:35:14 +0800

    Update MetaTransformHandler template struct implementation
    Update MetaTransformHandler template struct implementation

--- main.cpp
+++ main.cpp
@@ -978,7 +978,7 @@ public:
 
 //  MetaTransformHandler template struct implementation
 //  Generic Meta Handler strictly refactoring transform commands like abs, bicubic_resize, dct2, idct2, and lanczos_resample
-template <std::size_t MinArgs, typename CheckingTypes = master_image_types, typename SetupFun>
+template <std::size_t MinArgs, typename SetupFun, typename CheckingTypes = master_image_types>
 struct MetaTransformHandler
 {
     std::string_view usage_string_;
