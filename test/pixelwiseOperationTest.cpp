23ba808212f373a6c30b515fe9730844c8aafd46
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-16 00:37:41 +0800

    Update pixelwiseOperationTest.cpp
    Update pixelwiseOperationTest.cpp

--- test/pixelwiseOperationTest.cpp
+++ test/pixelwiseOperationTest.cpp
@@ -2,14 +2,15 @@
 #include "../basic_functions.h"
 #include "../image.h"
 #include "../image_operations.h"
+#include "../timer.h"
 
 template<typename T>
 void pixelwise_transformTest(const size_t size = 10)
 {
     auto img1 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
-	auto img2 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
-	auto img3 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
-	auto img4 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
+    auto img2 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
+    auto img3 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
+    auto img4 = TinyDIP::gaussianFigure2D(size, size, 5, 5, static_cast<T>(3));
 
     auto output = TinyDIP::pixelwise_transform
     (
@@ -27,13 +28,12 @@ void pixelwise_transformTest(const size_t size = 10)
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
+    TinyDIP::Timer timer1;
     pixelwise_transformTest<int>();
     pixelwise_transformTest<long>();
     pixelwise_transformTest<float>();
-	pixelwise_transformTest<double>();
-    auto end = std::chrono::system_clock::now();
-	return 0;
+    pixelwise_transformTest<double>();
+    return EXIT_SUCCESS;
 }
 
 
