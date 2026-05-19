73ab290b682ff70db1a44edee27fac3273d75280
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-20 02:18:34 +0800

    Remove HelpHandler struct implementation
    Remove HelpHandler struct implementation

--- main.cpp
+++ main.cpp
@@ -1874,22 +1874,6 @@ namespace handlers
 }
 
 
-//  HelpHandler struct implementation
-//  Wrapper for the 'help' functionality inside the REPL
-struct HelpHandler
-{
-    const CommandRegistry& registry_;
-
-    template <std::ranges::random_access_range ArgsT>
-    requires std::convertible_to<std::ranges::range_value_t<ArgsT>, std::string_view>
-    void operator()(const ArgsT& args, std::ostream& os = std::cout) const
-    {
-        //  Silencing unused parameter warning
-        (void)args;
-        registry_.list_commands(os);
-    }
-};
-
 //  InfoHandler struct implementation
 //  Wrapper for 'info' functionality
 //  Args: input_path
