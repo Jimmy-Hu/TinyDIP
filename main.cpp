d5751a47df12f29da33acb1f7474c158c22d8897
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-05-26 10:52:19 +0800

    Update make_meta_scalar_handler template function implementation
    Update make_meta_scalar_handler template function implementation

--- main.cpp
+++ main.cpp
@@ -1190,10 +1190,10 @@ struct MetaScalarHandler
 };
 
 //  make_meta_scalar_handler template function implementation
-template <std::size_t MinArgs, typename CheckingTypes = master_data_types, typename SetupFun>
+template <std::size_t MinArgs, typename CheckingTypes = master_data_types, typename SetupFun, typename ArgsContainer = std::vector<std::string_view>>
 constexpr auto make_meta_scalar_handler(std::string_view usage, std::string_view op_name, std::string_view capitalized_op_name, SetupFun&& setup)
 {
-    return MetaScalarHandler<MinArgs, std::remove_cvref_t<SetupFun>, CheckingTypes>{
+    return MetaScalarHandler<MinArgs, std::remove_cvref_t<SetupFun>, ArgsContainer, CheckingTypes>{
         usage, op_name, capitalized_op_name, std::forward<SetupFun>(setup)
     };
 }
