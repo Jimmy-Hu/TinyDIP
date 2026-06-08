a5189274befd51d7d2ca1d5196f8e7cd3575082a
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-08 17:49:51 +0800

    Update pixelwiseOperationTest.cpp
    Update pixelwiseOperationTest.cpp

--- test/pixelwiseOperationTest.cpp
+++ test/pixelwiseOperationTest.cpp
@@ -2,6 +2,7 @@
 #include "../basic_functions.h"
 #include "../image.h"
 #include "../image_operations.h"
+#include "../image_io.h"
 #include "../timer.h"
 
 template<typename T>
@@ -26,13 +27,52 @@ void pixelwise_transformTest(const size_t size = 10)
     output.print();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     TinyDIP::Timer timer1;
-    pixelwise_transformTest<int>();
-    pixelwise_transformTest<long>();
-    pixelwise_transformTest<float>();
-    pixelwise_transformTest<double>();
+    if (argc < 2)
+    {
+        pixelwise_transformTest<int>();
+        pixelwise_transformTest<long>();
+        pixelwise_transformTest<float>();
+        pixelwise_transformTest<double>();
+    }
+    else if (argc == 2)
+    {
+        std::filesystem::path input_path = std::string(argv[1]);
+        if (!std::filesystem::exists(input_path))
+        {
+            std::cerr << "File not found: " << input_path << '\n';
+            return EXIT_SUCCESS;
+        }
+        TinyDIP::Image<TinyDIP::RGB> input_img(0, 0);
+        if (input_path.extension() == ".bmp")
+        {
+            input_img = TinyDIP::bmp_read(input_path.string().c_str(), true);
+        }
+        else
+        {
+            input_img = TinyDIP::pnm::read(std::execution::par, input_path.string().c_str());
+        }
+        auto output_image = TinyDIP::pixelwise_transform(
+            [&](const auto& input_pixel)
+            {
+                auto r_value = input_pixel.channels[0];
+                auto g_value = input_pixel.channels[1];
+                auto b_value = input_pixel.channels[2];
+                if ((r_value >= 28) && (r_value <= 37) &&
+                    (g_value >= 40) && (g_value <= 57) &&
+                    (b_value >= 40) && (b_value <= 52))
+                {
+                    return TinyDIP::RGB{28, 28, 28};
+                }
+				return input_pixel;
+            },
+            input_img
+        );
+        TinyDIP::bmp_write("pixelwiseTransformOutput", output_image);
+    }
+
     return EXIT_SUCCESS;
 }
 
