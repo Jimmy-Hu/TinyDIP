d5d558c021cddb6ef8ef71863f8a2ebe250e3f56
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-08 18:05:50 +0800

    Add main.h
    Add main.h

---
+++ main.h
@@ -0,0 +1,50 @@
+#ifndef TINYDIP_MAIN_H
+#define TINYDIP_MAIN_H
+
+//  Standard Library Headers
+#include <algorithm>
+#include <any>
+#include <array>
+#include <charconv>
+#include <cmath>
+#include <complex>
+#include <concepts>
+#include <cstddef>
+#include <cstdint>
+#include <deque>
+#include <execution>
+#include <filesystem>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <numbers>
+#include <optional>
+#include <random>
+#include <ranges>
+#include <span>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+//  Local Headers
+#include "basic_functions.h"
+#include "image_io.h"
+#include "image_operations.h"
+#include "timer.h"
+
+
+
+
+#endif //TINYDIP_MAIN_H
