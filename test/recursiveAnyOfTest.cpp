a259afe32aefd11e1c2fa41b6a7ea729ff8d23a0
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-06-14 03:16:38 +0800

    Update recursiveAnyOfTest.cpp
    Update recursiveAnyOfTest.cpp

--- test/recursiveAnyOfTest.cpp
+++ test/recursiveAnyOfTest.cpp
@@ -7,6 +7,7 @@
 #include "../image.h"
 #include "../image_io.h"
 #include "../image_operations.h"
+#include "../timer.h"
 
 void recursive_any_of_tests()
 {
@@ -76,11 +77,7 @@ void recursive_any_of_tests()
 
 int main()
 {
-    auto start = std::chrono::system_clock::now();
+    TinyDIP::Timer timer1;
     recursive_any_of_tests();
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-    std::cout << "Computation finished at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds.count() << '\n';
     return EXIT_SUCCESS;
 }
