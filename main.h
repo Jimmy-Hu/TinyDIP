9086baa23f7ef45966c809ab9b3d57a535013352
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-07-12 20:22:56 +0800

    Update min template function implementation
    Update min template function implementation

--- main.h
+++ main.h
@@ -2599,10 +2599,15 @@ namespace handlers
         transform_handler(workspace, args, os);
     }
 
-    //  min function implementation
+    //  min template function implementation
+    template <std::ranges::input_range RangeT>
+    requires (std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string_view> or
+              std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string_view> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string>)
     constexpr void min(
         Workspace& workspace,
-        std::span<const std::string_view> args,
+        const RangeT& args,
         std::ostream& os = std::cout
     )
     {
