935f52c797346f7ff5b53fd5af8ecf29d7b0633c
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-07-12 20:29:38 +0800

    Update read template function implementation
    Update read template function implementation

--- main.h
+++ main.h
@@ -2954,12 +2954,17 @@ namespace handlers
 
 	//  read template function implementation
     template <
+        std::ranges::input_range RangeT,
         std::invocable<const std::string_view, Workspace&> ImageLoaderFun = MetaImageIO::Loader,
         std::invocable<const std::string_view, Workspace&, TinyDIP::Image<TinyDIP::RGB>&&> ImageSaverFun = MetaImageIO::Saver
     >
+    requires (std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string_view> or
+              std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string_view> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string>)
     constexpr void read(
         Workspace& workspace,
-        std::span<const std::string_view> args,
+        const RangeT& args,
         std::ostream& os = std::cout,
         ImageLoaderFun&& image_loader_fun = ImageLoaderFun{},
         ImageSaverFun&& image_saver_fun = ImageSaverFun{})
