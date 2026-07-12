6e9b5c8768a559b4ba9d5f04eb7a5d75c0890ed0
jimmyhu <5549662+Jimmy-Hu@users.noreply.github.com>
2026-07-12 20:36:50 +0800

    Update save_workspace template function implementation
    Update save_workspace template function implementation

--- main.h
+++ main.h
@@ -3013,10 +3013,15 @@ namespace handlers
         os << "Done.\n";
     }
 
-    //  save_workspace function implementation
+    //  save_workspace template function implementation
+    template <std::ranges::input_range RangeT>
+    requires (std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string_view> or
+              std::same_as<std::remove_cvref_t<std::ranges::range_value_t<RangeT>>, std::string> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string_view> or
+              std::convertible_to<std::ranges::range_value_t<RangeT>, std::string>)
     constexpr void save_workspace(
         Workspace& workspace,
-        std::span<const std::string_view> args,
+        const RangeT& args,
         std::ostream& os = std::cout)
     {
         if (std::ranges::size(args) < 1)
